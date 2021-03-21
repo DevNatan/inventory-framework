1fa2a5b321334d84d75a1d79caf9ae89b236081c
stooneg0mes <stoone.holy@gmail.com>
2021-03-21 01:42:09 -0300

    fix: cannot find symbol InventoryDragEvent

--- src/main/java/me/saiintbrisson/minecraft/ViewListener.java
+++ src/main/java/me/saiintbrisson/minecraft/ViewListener.java
@@ -5,6 +5,7 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.event.inventory.InventoryCloseEvent;
+import org.bukkit.event.inventory.InventoryDragEvent;
 import org.bukkit.event.inventory.InventoryType;
 import org.bukkit.event.player.PlayerDropItemEvent;
 import org.bukkit.event.player.PlayerPickupItemEvent;
