6227afa3546a0a723900553cda36b7c5feeb6356
Sasuked <majinboo.email@gmail.com>
2021-09-02 16:32:02 -0300

    fix: wrong view context size

--- src/main/java/me/saiintbrisson/minecraft/View.java
+++ src/main/java/me/saiintbrisson/minecraft/View.java
@@ -13,290 +13,303 @@ import java.util.function.Supplier;
 
 public class View extends VirtualView implements InventoryHolder, Closeable {
 
-    public static final int INVENTORY_ROW_SIZE = 9;
-    public static final int UNSET_SLOT = -1;
+	public static final int INVENTORY_ROW_SIZE = 9;
+	public static final int UNSET_SLOT = -1;
 
-    private ViewFrame frame;
-    private final String title;
-    private final int rows;
-    private final Map<Player, ViewContext> contexts;
-    private boolean cancelOnClick, cancelOnPickup, cancelOnDrop, cancelOnDrag, cancelOnClone, cancelOnMoveOut;
-    private final Map<Player, Map<String, Object>> data;
+	private ViewFrame frame;
+	private final String title;
+	private final int rows;
+	private final Map<Player, ViewContext> contexts;
+	private boolean cancelOnClick, cancelOnPickup, cancelOnDrop, cancelOnDrag, cancelOnClone, cancelOnMoveOut;
+	private final Map<Player, Map<String, Object>> data;
 
-    public View() {
-        this(1);
-    }
+	public View() {
+		this(1);
+	}
 
-    public View(final int rows) {
-        this(null, rows, "");
-    }
+	public View(final int rows) {
+		this(null, rows, "");
+	}
 
-    public View(final int rows, final String title) {
-        this(null, rows, title);
-    }
+	public View(final int rows, final String title) {
+		this(null, rows, title);
+	}
 
-    public View(final ViewFrame frame, final int rows, final String title) {
-        super(new ViewItem[INVENTORY_ROW_SIZE * rows]);
-        this.rows = rows;
-        this.frame = frame;
-        this.title = title;
-        contexts = new WeakHashMap<>();
-        data = new WeakHashMap<>();
-        cancelOnPickup = true;
-        cancelOnDrop = true;
-        cancelOnDrag = true;
-        cancelOnClone = true;
-        cancelOnMoveOut = true;
-    }
+	public View(final ViewFrame frame, final int rows, final String title) {
+		super(new ViewItem[INVENTORY_ROW_SIZE * rows]);
+		this.rows = rows;
+		this.frame = frame;
+		this.title = title;
+		contexts = new WeakHashMap<>();
+		data = new WeakHashMap<>();
+		cancelOnPickup = true;
+		cancelOnDrop = true;
+		cancelOnDrag = true;
+		cancelOnClone = true;
+		cancelOnMoveOut = true;
+	}
 
-    @Override
-    public int getLastSlot() {
-        return INVENTORY_ROW_SIZE * rows - 1;
-    }
+	@Override
+	public int getLastSlot() {
+		return INVENTORY_ROW_SIZE * rows - 1;
+	}
 
-    public Map<Player, ViewContext> getContexts() {
-        return contexts;
-    }
+	public Map<Player, ViewContext> getContexts() {
+		return contexts;
+	}
 
-    public ViewContext getContext(final Player player) {
-        return contexts.get(player);
-    }
+	public ViewContext getContext(final Player player) {
+		return contexts.get(player);
+	}
 
-    public ViewFrame getFrame() {
-        return frame;
-    }
+	public ViewFrame getFrame() {
+		return frame;
+	}
 
-    void setFrame(final ViewFrame frame) {
-        this.frame = frame;
-    }
+	void setFrame(final ViewFrame frame) {
+		this.frame = frame;
+	}
 
-    public int getRows() {
-        return rows;
-    }
+	public int getRows() {
+		return rows;
+	}
 
-    public String getTitle() {
-        return title;
-    }
+	public String getTitle() {
+		return title;
+	}
 
-    protected ViewContext createContext(final View view, final Player player, final Inventory inventory) {
-        return new ViewContext(view, player, inventory);
-    }
+	protected ViewContext createContext(final View view, final Player player, final Inventory inventory) {
+		return new ViewContext(view, player, inventory);
+	}
 
-    public void open(final Player player) {
-        open(player, null);
-    }
+	public void open(final Player player) {
+		open(player, null);
+	}
 
-    public void open(final Player player, final Map<String, Object> data) {
-        if (contexts.containsKey(player))
-            return;
+	public void open(final Player player, final Map<String, Object> data) {
+		if (contexts.containsKey(player))
+			return;
 
-        final OpenViewContext preOpenContext = new OpenViewContext(this, player);
-        if (data != null)
-            setData(player, new HashMap<>(data));
-        else {
-            // ensure non-transitive data on view switch
-            clearData(player);
-        }
+		final OpenViewContext preOpenContext = new OpenViewContext(this, player);
+		if (data != null)
+			setData(player, new HashMap<>(data));
+		else {
+			// ensure non-transitive data on view switch
+			clearData(player);
+		}
 
-        onOpen(preOpenContext);
-        if (preOpenContext.isCancelled()) {
-            clearData(player);
-            return;
-        }
+		onOpen(preOpenContext);
+		if (preOpenContext.isCancelled()) {
+			clearData(player);
+			return;
+		}
 
-        final Inventory inventory = getInventory(preOpenContext.getInventoryTitle(), preOpenContext.getInventorySize());
-        final ViewContext context = createContext(this, player, inventory);
-        contexts.put(player, context);
-        onRender(context);
-        render(context);
-        player.openInventory(inventory);
-    }
+		int inventorySize = preOpenContext.getInventorySize();
+		if (inventorySize != items.length) {
+			this.expandItemsArray(inventorySize);
+		}
 
-    public void update() {
-        for (final ViewContext ctx : contexts.values())
-            ctx.update();
-    }
+		final Inventory inventory = getInventory(preOpenContext.getInventoryTitle(), inventorySize);
+		final ViewContext context = createContext(this, player, inventory);
+		contexts.put(player, context);
+		onRender(context);
+		render(context);
+		player.openInventory(inventory);
+	}
 
-    @Override
-    public void update(final ViewContext context) {
-        frame.debug("[context]: update");
-        onUpdate(context);
-        super.update(context);
-    }
+	public void update() {
+		for (final ViewContext ctx : contexts.values())
+			ctx.update();
+	}
 
-    @Override
-    public void update(final ViewContext context, final int slot) {
-        frame.debug("[slot " + slot + "]: update");
-        super.update(context, slot);
-    }
+	@Override
+	public void update(final ViewContext context) {
+		frame.debug("[context]: update");
+		onUpdate(context);
+		super.update(context);
+	}
 
-    @Override
-    public void render(final ViewContext context) {
-        frame.debug("[context]: render");
-        super.render(context);
-    }
+	@Override
+	public void update(final ViewContext context, final int slot) {
+		frame.debug("[slot " + slot + "]: update");
+		super.update(context, slot);
+	}
 
-    @Override
-    public void render(final ViewContext context, final int slot) {
-        frame.debug("[slot " + slot + "]: render");
-        super.render(context, slot);
-    }
+	@Override
+	public void render(final ViewContext context) {
+		frame.debug("[context]: render");
+		super.render(context);
+	}
 
-    @Override
-    public void render(final ViewContext context, final ViewItem item, final int slot) {
-        frame.debug("[slot " + slot + "]: render with item");
-        super.render(context, item, slot);
-    }
+	@Override
+	public void render(final ViewContext context, final int slot) {
+		frame.debug("[slot " + slot + "]: render");
+		super.render(context, slot);
+	}
 
-    @Override
-    ViewItem resolve(final ViewContext context, final int slot) {
-        frame.debug("[slot " + slot + "]: resolve item");
-        return super.resolve(context, slot);
-    }
+	@Override
+	public void render(final ViewContext context, final ViewItem item, final int slot) {
+		frame.debug("[slot " + slot + "]: render with item");
+		super.render(context, item, slot);
+	}
 
-    ViewContext remove(final Player player) {
-        frame.debug("[context]: remove");
-        final ViewContext context = contexts.remove(player);
-        if (context != null) {
-            context.invalidate();
-            frame.debug("[context]: invalidate");
-        }
+	@Override
+	ViewItem resolve(final ViewContext context, final int slot) {
+		frame.debug("[slot " + slot + "]: resolve item");
+		return super.resolve(context, slot);
+	}
 
-        return context;
-    }
+	ViewContext remove(final Player player) {
+		frame.debug("[context]: remove");
+		final ViewContext context = contexts.remove(player);
+		if (context != null) {
+			context.invalidate();
+			frame.debug("[context]: invalidate");
+		}
 
-    void remove(final ViewContext context) {
-        frame.debug("[context]: remove");
-        contexts.remove(context.getPlayer());
-        context.invalidate();
-        frame.debug("[context]: invalidate");
-    }
+		return context;
+	}
 
-    public void close() {
-        for (final Player player : contexts.keySet()) {
-            player.closeInventory();
-        }
-    }
+	void remove(final ViewContext context) {
+		frame.debug("[context]: remove");
+		contexts.remove(context.getPlayer());
+		context.invalidate();
+		frame.debug("[context]: invalidate");
+	}
 
-    public boolean isCancelOnClick() {
-        return cancelOnClick;
-    }
+	public void close() {
+		for (final Player player : contexts.keySet()) {
+			player.closeInventory();
+		}
+	}
 
-    public void setCancelOnClick(final boolean cancelOnClick) {
-        this.cancelOnClick = cancelOnClick;
-    }
+	public boolean isCancelOnClick() {
+		return cancelOnClick;
+	}
 
-    public boolean isCancelOnPickup() {
-        return cancelOnPickup;
-    }
+	public void setCancelOnClick(final boolean cancelOnClick) {
+		this.cancelOnClick = cancelOnClick;
+	}
 
-    public void setCancelOnPickup(final boolean cancelOnPickup) {
-        this.cancelOnPickup = cancelOnPickup;
-    }
+	public boolean isCancelOnPickup() {
+		return cancelOnPickup;
+	}
 
-    public boolean isCancelOnDrop() {
-        return cancelOnDrop;
-    }
+	public void setCancelOnPickup(final boolean cancelOnPickup) {
+		this.cancelOnPickup = cancelOnPickup;
+	}
 
-    public void setCancelOnDrop(final boolean cancelOnDrop) {
-        this.cancelOnDrop = cancelOnDrop;
-    }
+	public boolean isCancelOnDrop() {
+		return cancelOnDrop;
+	}
 
-    public boolean isCancelOnDrag() {
-        return cancelOnDrag;
-    }
+	public void setCancelOnDrop(final boolean cancelOnDrop) {
+		this.cancelOnDrop = cancelOnDrop;
+	}
 
-    public void setCancelOnDrag(final boolean cancelOnDrag) {
-        this.cancelOnDrag = cancelOnDrag;
-    }
+	public boolean isCancelOnDrag() {
+		return cancelOnDrag;
+	}
 
-    public boolean isCancelOnClone() {
-        return cancelOnClone;
-    }
+	public void setCancelOnDrag(final boolean cancelOnDrag) {
+		this.cancelOnDrag = cancelOnDrag;
+	}
 
-    public void setCancelOnClone(final boolean cancelOnClone) {
-        this.cancelOnClone = cancelOnClone;
-    }
+	public boolean isCancelOnClone() {
+		return cancelOnClone;
+	}
 
-    public boolean isCancelOnMoveOut() {
-        return cancelOnMoveOut;
-    }
+	public void setCancelOnClone(final boolean cancelOnClone) {
+		this.cancelOnClone = cancelOnClone;
+	}
 
-    public void setCancelOnMoveOut(boolean cancelOnMoveOut) {
-        this.cancelOnMoveOut = cancelOnMoveOut;
-    }
+	public boolean isCancelOnMoveOut() {
+		return cancelOnMoveOut;
+	}
 
-    @Override
-    public Inventory getInventory() {
-        throw new UnsupportedOperationException();
-    }
+	public void setCancelOnMoveOut(boolean cancelOnMoveOut) {
+		this.cancelOnMoveOut = cancelOnMoveOut;
+	}
 
-    private Inventory getInventory(final String title, final int size) {
-        return Bukkit.createInventory(this, size, title == null ? this.title : title);
-    }
+	@Override
+	public Inventory getInventory() {
+		throw new UnsupportedOperationException();
+	}
 
-    public void clearData(final Player player) {
-        data.remove(player);
-    }
+	private Inventory getInventory(final String title, final int size) {
+		return Bukkit.createInventory(this, size, title == null ? this.title : title);
+	}
 
-    public void clearData(final Player player, final String key) {
-        if (!data.containsKey(player))
-            return;
+	public void clearData(final Player player) {
+		data.remove(player);
+	}
 
-        data.get(player).remove(key);
-    }
+	public void clearData(final Player player, final String key) {
+		if (!data.containsKey(player))
+			return;
 
-    public Map<String, Object> getData(final Player player) {
-        return data.get(player);
-    }
+		data.get(player).remove(key);
+	}
 
-    @SuppressWarnings("unchecked")
-    public <T> T getData(final Player player, final String key) {
-        if (!data.containsKey(player))
-            return null;
+	public Map<String, Object> getData(final Player player) {
+		return data.get(player);
+	}
 
-        return (T) data.get(player).get(key);
-    }
+	@SuppressWarnings("unchecked")
+	public <T> T getData(final Player player, final String key) {
+		if (!data.containsKey(player))
+			return null;
 
-    @SuppressWarnings("unchecked")
-    public <T> T getData(final Player player, final String key, final Supplier<T> defaultValue) {
-        if (!data.containsKey(player) || !data.get(player).containsKey(key))
-            return defaultValue.get();
+		return (T) data.get(player).get(key);
+	}
 
-        return (T) data.get(player).get(key);
-    }
+	@SuppressWarnings("unchecked")
+	public <T> T getData(final Player player, final String key, final Supplier<T> defaultValue) {
+		if (!data.containsKey(player) || !data.get(player).containsKey(key))
+			return defaultValue.get();
 
-    public void setData(final Player player, final Map<String, Object> data) {
-        this.data.put(player, data);
-    }
+		return (T) data.get(player).get(key);
+	}
 
-    public void setData(final Player player, final String key, final Object value) {
-        data.computeIfAbsent(player, $ -> new HashMap<>()).put(key, value);
-    }
+	public void setData(final Player player, final Map<String, Object> data) {
+		this.data.put(player, data);
+	}
 
-    public boolean hasData(final Player player, final String key) {
-        if (!data.containsKey(player))
-            return false;
+	public void setData(final Player player, final String key, final Object value) {
+		data.computeIfAbsent(player, $ -> new HashMap<>()).put(key, value);
+	}
 
-        return data.get(player).containsKey(key);
-    }
+	public boolean hasData(final Player player, final String key) {
+		if (!data.containsKey(player))
+			return false;
 
-    protected void onOpen(final OpenViewContext context) {
-    }
+		return data.get(player).containsKey(key);
+	}
 
-    protected void onRender(final ViewContext context) {
-    }
+	protected void onOpen(final OpenViewContext context) {
+	}
 
-    protected void onClose(final ViewContext context) {
-    }
+	protected void onRender(final ViewContext context) {
+	}
 
-    protected void onClick(final ViewSlotContext context) {
-    }
+	protected void onClose(final ViewContext context) {
+	}
 
-    protected void onUpdate(final ViewContext context) {
-    }
+	protected void onClick(final ViewSlotContext context) {
+	}
 
-    protected void onMoveOut(final ViewSlotMoveContext context) {
-    }
+	protected void onUpdate(final ViewContext context) {
+	}
+
+	protected void onMoveOut(final ViewSlotMoveContext context) {
+	}
+
+
+	private void expandItemsArray(int newLength) {
+		ViewItem[] newItems = new ViewItem[items.length + 1];
+		System.arraycopy(items, 0, newItems, 0, items.length);
+
+		items = newItems;
+	}
 
 }
