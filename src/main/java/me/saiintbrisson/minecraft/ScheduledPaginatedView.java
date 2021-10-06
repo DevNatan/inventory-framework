a0e981f545cc7da24c04db225d3c2c4413c5c2cc
Natan Vieira <natanvnascimento@gmail.com>
2021-10-06 20:56:22 -0300

    Revert "feat: add `setPeriod` and `setDelay` to `ScheduledPaginatedView`"
    This reverts commit fbe06e407195a6ab0f5e0ac40b939ac17e750b9e.

--- src/main/java/me/saiintbrisson/minecraft/ScheduledPaginatedView.java
+++ src/main/java/me/saiintbrisson/minecraft/ScheduledPaginatedView.java
@@ -5,33 +5,15 @@ import org.bukkit.scheduler.BukkitTask;
 
 public abstract class ScheduledPaginatedView<T> extends PaginatedView<T> {
 
-	private long delay = 1L;
-	private long period = 1L;
-
 	private BukkitTask task;
-
-	public final long getDelay() {
-		return delay;
-	}
-
-	protected void setDelay(long delay) {
-		this.delay = delay;
-	}
-
-	public final long getPeriod() {
-		return period;
-	}
-
-	protected void setPeriod(long period) {
-		this.period = period;
-	}
 
 	@Override
 	protected void onOpen(OpenViewContext ctx) {
 		if (getContexts().isEmpty()) {
-			task = Bukkit.getScheduler().runTaskTimer(ctx.getView().getFrame().getOwner(), this::update, delay, period);
+			task = Bukkit.getScheduler().runTaskTimer(ctx.getView().getFrame().getOwner(), this::update, 1L, 1L);
 		}
 	}
+
 
 	@Override
 	protected void onClose(ViewContext context) {
@@ -40,5 +22,4 @@ public abstract class ScheduledPaginatedView<T> extends PaginatedView<T> {
 			task = null;
 		}
 	}
-
 }
