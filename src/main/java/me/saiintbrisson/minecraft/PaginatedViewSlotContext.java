db930201f616c2080f13c80ef2d66d8cdf2cb3f9
Natan Vieira <natanvnascimento@gmail.com>
2021-10-09 23:23:33 -0300

    fix: delegate items from delegated PaginatedViewContext

--- src/main/java/me/saiintbrisson/minecraft/PaginatedViewSlotContext.java
+++ src/main/java/me/saiintbrisson/minecraft/PaginatedViewSlotContext.java
@@ -2,11 +2,13 @@ package me.saiintbrisson.minecraft;
 
 public class PaginatedViewSlotContext<T> extends PaginatedViewContext<T> {
 
+    private final PaginatedViewContext<T> delegate;
     private final int index;
     private final int slot;
 
     public PaginatedViewSlotContext(PaginatedViewContext<T> delegate, int index, int slot) {
         super(delegate.getView(), delegate.getPlayer(), delegate.getInventory(), delegate.getPage());
+        this.delegate = delegate;
         this.index = index;
         this.slot = slot;
     }
@@ -27,6 +29,19 @@ public class PaginatedViewSlotContext<T> extends PaginatedViewContext<T> {
      */
     public int getSlot() {
         return slot;
+    }
+
+    @Override
+    public ViewItem[] getItems() {
+        return delegate.getItems();
+    }
+
+    @Override
+    public String toString() {
+        return "PaginatedViewSlotContext{" +
+                "index=" + index +
+                ", slot=" + slot +
+                "} " + super.toString();
     }
 
 }
