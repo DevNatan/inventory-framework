c0083f9e7437a9d17c46c5c74e9d6a6bdf4d256d
stooneg0mes <stoone.holy@gmail.com>
2021-12-30 20:02:11 -0300

    feat: set custom period/delay for scheduled views

--- src/main/java/me/saiintbrisson/minecraft/ScheduledPaginatedView.java
+++ src/main/java/me/saiintbrisson/minecraft/ScheduledPaginatedView.java
@@ -1,17 +1,24 @@
 package me.saiintbrisson.minecraft;
 
 import org.bukkit.Bukkit;
+import org.bukkit.plugin.Plugin;
 import org.bukkit.scheduler.BukkitTask;
 
 public abstract class ScheduledPaginatedView<T> extends PaginatedView<T> implements Runnable {
 
+	private final Plugin plugin = getFrame().getOwner();
+
 	private BukkitTask task;
+	private Long delay, period;
 
 	@Override
 	public void onOpen(OpenViewContext context) {
 		if (getContexts().size() != 1) return;
 
-		task = Bukkit.getScheduler().runTaskTimer(context.getView().getFrame().getOwner(), this, 1L, 1L);
+		if (delay == null) delay = 1L;
+		if (period == null) period = 1L;
+
+		task = Bukkit.getScheduler().runTaskTimer(plugin, this, 1L, 1L);
 	}
 
 	@Override
@@ -25,6 +32,18 @@ public abstract class ScheduledPaginatedView<T> extends PaginatedView<T> impleme
 	@Override
 	public final void run() {
 		update();
+	}
+
+	/*
+	Delay & period
+	 */
+
+	public void setDelay(long delay) {
+		this.delay = delay;
+	}
+
+	public void setPeriod(long period) {
+		this.period = period;
 	}
 
 }
