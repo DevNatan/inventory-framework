103c4b955b826b5e33775670198634ffb9b8cedb
Natan Vieira do Nascimento <natanvnascimento@gmail.com>
2022-02-12 18:39:01 -0300

    Add documentation to View handlers

--- src/main/java/me/saiintbrisson/minecraft/View.java
+++ src/main/java/me/saiintbrisson/minecraft/View.java
@@ -18,16 +18,14 @@ public class View extends VirtualView implements InventoryHolder, Closeable {
 	public static final int INVENTORY_ROW_SIZE = 9;
 	public static final int UNSET_SLOT = -1;
 	public static final int DEFAULT_INVENTORY_ROW_COUNT = 3;
-
+	private static final Set<String> ENABLED_FEATURES = new HashSet<>();
 	private final String title;
 	private final int rows;
-
 	private final Map<String, ViewContext> contexts;
 	private final Map<Player, Map<String, Object>> data;
 	private ViewFrame frame;
 	private boolean cancelOnClick, cancelOnPickup, cancelOnDrop, cancelOnDrag, cancelOnClone;
 	private boolean cancelOnMoveOut, cancelOnShiftClick, clearCursorOnClose, closeOnOutsideClick;
-	private static final Set<String> ENABLED_FEATURES = new HashSet<>();
 
 	public View() {
 		this(0);
@@ -54,6 +52,14 @@ public class View extends VirtualView implements InventoryHolder, Closeable {
 		cancelOnClone = true;
 		cancelOnMoveOut = true;
 		cancelOnShiftClick = true;
+	}
+
+	public static void enableFeaturePreview(@NotNull String feature) {
+		ENABLED_FEATURES.add(feature);
+	}
+
+	static boolean isFeatureEnabled(@NotNull String feature) {
+		return ENABLED_FEATURES.contains(feature);
 	}
 
 	public final Map<Player, ViewContext> getContexts() {
@@ -339,36 +345,135 @@ public class View extends VirtualView implements InventoryHolder, Closeable {
 	}
 
 	/**
+	 * Called before the inventory is opened to the player.
+	 * <p>
+	 * This handler is often called "pre-rendering" because it is possible to set
+	 * the title and size of the inventory and also cancel the opening of the View
+	 * without even doing any handling related to the inventory.
+	 * <p>
+	 * It is not possible to manipulate the inventory in this handler, if it
+	 * happens a exception will be thrown.
 	 *
-	 *
-	 *
-	 * @param context The player opening view context.
+	 * @param context The player view context.
 	 */
 	protected void onOpen(@NotNull OpenViewContext context) {
 	}
 
+	/**
+	 * Called when this view is rendered to the player.
+	 * <p>
+	 * This is where you will define items that will be contained
+	 * non-persistently in the context.
+	 * <p>
+	 * Using {@link View#slot(int)} here will cause a leak of items in memory
+	 * or that  the item that was previously defined will be overwritten as
+	 * the slot item definition method is for use in the constructor only
+	 * once. Instead, you should use the context item definition function
+	 * {@link ViewContext#slot(int)}.
+	 * <p>
+	 * Handlers call order:
+	 * <ul>
+	 *     <li>{@link #onOpen(OpenViewContext)}</li>
+	 *     <li>this rendering function</li>
+	 *     <li>{@link #onUpdate(ViewContext)}</li>
+	 *     <li>{@link #onClose(ViewContext)}</li>
+	 * </ul>
+	 * <p>
+	 * This is a rendering function and can modify the view's inventory.
+	 *
+	 * @param context The player view context.
+	 */
 	protected void onRender(@NotNull ViewContext context) {
 	}
 
+	/**
+	 * Called when the player closes the view's inventory.
+	 * <p>
+	 * It is possible to cancel this event and have the view's inventory open
+	 * again for the player.
+	 *
+	 * @param context The player view context.
+	 */
 	protected void onClose(@NotNull ViewContext context) {
 	}
 
+	/**
+	 * Called when a player clicks on the view inventory.
+	 * <p>
+	 * This function is called even if the click has been cancelled,
+	 * you can check this using {@link ViewSlotContext#isCancelled()}.
+	 * <p>
+	 * Canceling the context will cancel the click.
+	 * <p>
+	 * Handling the inventory in the click handler is not allowed.
+	 *
+	 * @param context The player view context.
+	 */
 	protected void onClick(@NotNull ViewSlotContext context) {
 	}
 
+	/**
+	 * Called when the view is updated for a player.
+	 * <p>
+	 * This is a rendering function and can modify the view's inventory.
+	 *
+	 * @param context The player view context.
+	 * @see View#update()
+	 * @see ViewContext#update()
+	 */
 	protected void onUpdate(@NotNull ViewContext context) {
 	}
 
+	/**
+	 * Called when a player moves a view item out of the view's inventory.
+	 * <p>
+	 * Canceling the context will cancel the move.
+	 * Don't confuse moving with dropping.
+	 *
+	 * @param context The player view context.
+	 */
 	protected void onMoveOut(@NotNull ViewSlotMoveContext context) {
 	}
 
 	protected void onMoveIn(@NotNull ViewSlotMoveContext context) {
 	}
 
+	/**
+	 * Called when the player holds an item in the inventory.
+	 * <p>
+	 * This handler will only work if the player manages to successfully hold
+	 * the item, for example it will not be called if the click has been
+	 * canceled for whatever reasons.
+	 * <p>
+	 * This context is non-cancelable.
+	 *
+	 * @param context The player view context.
+	 */
 	protected void onItemHold(@NotNull ViewSlotContext context) {
 	}
 
-	protected void onItemRelease(@NotNull ViewSlotContext from, @NotNull ViewSlotContext to) {
+	/**
+	 * Called when an item is dropped by the player in an inventory
+	 * (not necessarily the View's inventory).
+	 * <p>
+	 * With this it is possible to detect if the player held and released an
+	 * item:
+	 * <ul>
+	 *     <li>inside the view</li>
+	 *     <li>outside the view (in the player inventory)</li>
+	 *     <li>from inside to outside the view (to the player inventory)</li>
+	 *     <li>from outside to inside the view (from the player inventory)</li>
+	 * </ul>
+	 * <p>
+	 * This handler is the counterpart of {@link #onItemHold(ViewSlotContext)}.
+	 *
+	 * @param from The input context of the move.
+	 * @param to   The output context of the move.
+	 */
+	protected void onItemRelease(
+		@NotNull ViewSlotContext from,
+		@NotNull ViewSlotContext to
+	) {
 	}
 
 	private void expandItemsArray(int newLength) {
@@ -376,14 +481,6 @@ public class View extends VirtualView implements InventoryHolder, Closeable {
 		System.arraycopy(items, 0, newItems, 0, items.length);
 
 		items = newItems;
-	}
-
-	public static void enableFeaturePreview(@NotNull String feature) {
-		ENABLED_FEATURES.add(feature);
-	}
-
-	static boolean isFeatureEnabled(@NotNull String feature) {
-		return ENABLED_FEATURES.contains(feature);
 	}
 
 	@Override
