f3b91e5372fff0e9f3264488724785813add99a9
Natan Vieira <natanvnascimento@gmail.com>
2022-05-11 23:42:28 -0300

    Prohibit inventory modification on click context

---
+++ framework/src/main/java/me/saiintbrisson/minecraft/ClickSlotContext.java
@@ -0,0 +1,35 @@
+package me.saiintbrisson.minecraft;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.bukkit.event.inventory.InventoryClickEvent;
+import org.jetbrains.annotations.NotNull;
+
+@Getter
+@Setter
+@ToString(callSuper = true)
+class ClickSlotContext extends BaseViewContext implements ViewSlotContext {
+
+	private final InventoryClickEvent clickOrigin;
+
+	@Setter
+	private boolean cancelled;
+
+	public ClickSlotContext(
+		@NotNull ViewContext context,
+		@NotNull InventoryClickEvent clickOrigin
+	) {
+		super(context);
+		this.clickOrigin = clickOrigin;
+	}
+
+	@Override
+	void inventoryAccessNeeded() {
+		throw new IllegalStateException(
+			"You cannot modify the inventory in the click context. " +
+			"Use the onRender(...) rendering function for this."
+		);
+	}
+
+}
