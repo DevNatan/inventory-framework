de8ce9bcc99eab2caedfb0a4baab15a7296e48b7
Natan Vieira <itroken99@gmail.com>
2022-05-15 02:03:24 -0300

    Add test to fallback bukkit component factory

---
+++ framework/bukkit/src/test/java/me/saiintbrisson/minecraft/BukkitViewComponentFactoryRetrievalTest.java
@@ -0,0 +1,15 @@
+package me.saiintbrisson.minecraft;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class BukkitViewComponentFactoryRetrievalTest {
+
+	@Test
+	void shouldRetrieveBukkitFactoryByDefault() {
+		PlatformUtils.removeFactory();
+		ViewComponentFactory factory = Assertions.assertDoesNotThrow(PlatformUtils::getFactory);
+		Assertions.assertTrue(factory instanceof BukkitViewComponentFactory);
+	}
+
+}
