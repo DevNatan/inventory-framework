bb0da635636694f5e7b0748329344847a7f92414
Natan Vieira <itroken99@gmail.com>
2022-06-08 13:18:18 -0300

    HotbarClickInterceptor docs

--- framework/bukkit-runtime/src/main/java/me/saiintbrisson/minecraft/HotbarClickInterceptor.java
+++ framework/bukkit-runtime/src/main/java/me/saiintbrisson/minecraft/HotbarClickInterceptor.java
@@ -3,11 +3,19 @@ package me.saiintbrisson.minecraft;
 import org.bukkit.event.inventory.ClickType;
 import org.bukkit.event.inventory.InventoryClickEvent;
 
+/**
+ * Intercepts the player's hotbar click by launching
+ * {@link View#onHotbarInteract(ViewContext, int)} when it happens.
+ *
+ * @see PipelineInterceptor
+ */
 public final class HotbarClickInterceptor implements PipelineInterceptor<BukkitClickViewSlotContext> {
 
 	@Override
-	public void intercept(PipelineContext<BukkitClickViewSlotContext> pipeline, BukkitClickViewSlotContext subject) {
-		System.out.println("intercepted hotbar click");
+	public void intercept(
+		PipelineContext<BukkitClickViewSlotContext> pipeline,
+		BukkitClickViewSlotContext subject
+	) {
 		final InventoryClickEvent clickEvent = subject.getClickOrigin();
 
 		if (clickEvent.getClick() != ClickType.NUMBER_KEY)
