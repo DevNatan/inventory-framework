609d1907bae7b1e48264baa40d3164bced9ca719
Natan Vieira <itroken99@gmail.com>
2022-06-15 15:39:42 -0300

    Test error handler

---
+++ framework/src/test/java/me/saiintbrisson/minecraft/ErrorHandlerTest.java
@@ -0,0 +1,112 @@
+package me.saiintbrisson.minecraft;
+
+import org.jetbrains.annotations.NotNull;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.Collection;
+import java.util.Map;
+import java.util.function.UnaryOperator;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class ErrorHandlerTest {
+
+	@Test
+	void shouldCatchException() {
+		AbstractView view = new AbstractView(0, null, ViewType.CHEST) {
+		};
+		view.setErrorHandler(($, exception) -> assertTrue(exception instanceof IllegalStateException));
+
+		assertDoesNotThrow(() -> view.runCatching(null, () -> {
+			throw new IllegalStateException();
+		}));
+	}
+
+	@Test
+	void shouldCatchUnhandledException() {
+		AbstractView view = new AbstractView(0, null, ViewType.CHEST) {
+		};
+
+		try {
+			view.runCatching(null, () -> {
+				throw new IllegalStateException();
+			});
+			Assertions.fail();
+		} catch (IllegalStateException ignored) {
+		}
+	}
+
+	@Test
+	void shouldContextUnhandledException() {
+		AbstractView view = new AbstractView(0, null, ViewType.CHEST) {
+		};
+		BaseViewContext context = new BaseViewContext(view, null);
+
+		try {
+			view.runCatching(context, () -> {
+				throw new IllegalStateException();
+			});
+			Assertions.fail();
+		} catch (IllegalStateException ignored) {
+		}
+	}
+
+	@Test
+	void shouldHandleContextException() {
+		AbstractView view = new AbstractView(0, null, ViewType.CHEST) {
+		};
+		BaseViewContext context = new BaseViewContext(view, null);
+		context.setErrorHandler(($, exception) -> assertTrue(exception instanceof IllegalStateException));
+
+		assertDoesNotThrow(() -> view.runCatching(context, () -> {
+			throw new IllegalStateException();
+		}));
+	}
+
+	@Test
+	void shouldPropagateContextException() {
+		AbstractView view = new AbstractView(0, null, ViewType.CHEST) {
+		};
+		view.setErrorHandler(($, exception) -> assertTrue(exception instanceof IllegalStateException));
+
+		BaseViewContext context = new BaseViewContext(view, null);
+		context.setErrorHandler(($, exception) -> assertTrue(exception instanceof IllegalStateException));
+
+		assertDoesNotThrow(() -> view.runCatching(context, () -> {
+			throw new IllegalStateException();
+		}));
+	}
+
+	@Test
+	void shouldCancelContextExceptionPropagation() {
+		AbstractView view = new AbstractView(0, null, ViewType.CHEST) {
+		};
+		view.setErrorHandler(($, exception) -> Assertions.fail());
+
+		BaseViewContext context = new BaseViewContext(view, null);
+		context.setPropagateErrors(false);
+		context.setErrorHandler(($, exception) -> assertTrue(exception instanceof IllegalStateException));
+
+		assertDoesNotThrow(() -> view.runCatching(context, () -> {
+			throw new IllegalStateException();
+		}));
+	}
+
+	@Test
+	void shouldPropagateContextExceptionToViewFrame() {
+		PlatformViewFrame<?, ?, ?> vf = new TestViewFrame();
+		vf.setErrorHandler(($, exception) -> assertTrue(exception instanceof IllegalStateException));
+
+		AbstractView view = new AbstractView(0, null, ViewType.CHEST) {
+		};
+		view.setViewFrame(vf);
+
+		BaseViewContext context = new BaseViewContext(view, null);
+		assertDoesNotThrow(() -> view.runCatching(context, () -> {
+			throw new IllegalStateException();
+		}));
+	}
+
+}
---
+++ framework/src/test/java/me/saiintbrisson/minecraft/TestViewFrame.java
@@ -0,0 +1,77 @@
+package me.saiintbrisson.minecraft;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Collection;
+import java.util.Map;
+import java.util.function.UnaryOperator;
+
+public class TestViewFrame implements PlatformViewFrame<Void, Void, TestViewFrame> {
+
+	@Getter
+	@Setter
+	private ViewErrorHandler errorHandler;
+
+	@Override
+	public @NotNull Void getPlatform() {
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public Collection<Feature<?, ?>> getInstalledFeatures() {
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public <C, R> @NotNull R install(@NotNull Feature<C, R> feature, @NotNull UnaryOperator<C> configure) {
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public TestViewFrame with(@NotNull View... views) {
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public TestViewFrame remove(@NotNull View... views) {
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public TestViewFrame register() {
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public void unregister() {
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public boolean isRegistered() {
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public @NotNull Void getOwner() {
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public @NotNull ViewComponentFactory getFactory() {
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public <R extends AbstractView> R open(@NotNull Class<R> viewClass, @NotNull Void viewer) {
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public <R extends AbstractView> R open(@NotNull Class<R> viewClass, @NotNull Void viewer, @NotNull Map<String, Object> data) {
+		throw new UnsupportedOperationException();
+	}
+
+}
