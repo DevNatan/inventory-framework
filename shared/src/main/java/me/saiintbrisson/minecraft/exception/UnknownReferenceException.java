9eaf41f857a46eab350da8e22bc644b68d477162
Natan Vieira <natanvnascimento@gmail.com>
2022-08-23 18:35:22 -0300

    Custom UnknownReferenceException and nullable ref (#210)
    Implements a not so generic exception to `ref` and allow users use `refOrNull(key)` instead of throwing an exception for unknown references

---
+++ shared/src/main/java/me/saiintbrisson/minecraft/exception/UnknownReferenceException.java
@@ -0,0 +1,7 @@
+package me.saiintbrisson.minecraft.exception;
+
+public final class UnknownReferenceException extends InventoryFrameworkException {
+    public UnknownReferenceException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
