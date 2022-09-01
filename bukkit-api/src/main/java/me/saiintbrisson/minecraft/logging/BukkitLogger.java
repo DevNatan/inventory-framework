4d6ced0860072e28ee7c9c800fd333970652556d
Natan Vieira <itroken99@gmail.com>
2022-09-01 17:35:10 -0300

    Lint codebase

--- bukkit-api/src/main/java/me/saiintbrisson/minecraft/logging/BukkitLogger.java
+++ bukkit-api/src/main/java/me/saiintbrisson/minecraft/logging/BukkitLogger.java
@@ -6,34 +6,34 @@ import org.jetbrains.annotations.Nullable;
 @RequiredArgsConstructor
 public final class BukkitLogger implements Logger {
 
-	private final java.util.logging.Logger logger;
-	private final String viewName;
-	private final boolean isShaded;
+    private final java.util.logging.Logger logger;
+    private final String viewName;
+    private final boolean isShaded;
 
-	private String finalPrefix;
+    private String finalPrefix;
 
-	@Override
-	@Nullable
-	public String getPrefix() {
-		if (finalPrefix == null) {
-			finalPrefix = String.format("%s[%s]", isShaded ? "[IF]" : "", viewName) + " ";
-		}
+    @Override
+    @Nullable
+    public String getPrefix() {
+        if (finalPrefix == null) {
+            finalPrefix = String.format("%s[%s]", isShaded ? "[IF]" : "", viewName) + " ";
+        }
 
-		return finalPrefix;
-	}
+        return finalPrefix;
+    }
 
-	@Override
-	public void debug(String message) {
-		logger.info(getPrefix() + message);
-	}
+    @Override
+    public void debug(String message) {
+        logger.info(getPrefix() + message);
+    }
 
-	@Override
-	public void warn(String message) {
-		logger.warning(getPrefix() + message);
-	}
+    @Override
+    public void warn(String message) {
+        logger.warning(getPrefix() + message);
+    }
 
-	@Override
-	public void error(String message) {
-		logger.severe(getPrefix() + message);
-	}
+    @Override
+    public void error(String message) {
+        logger.severe(getPrefix() + message);
+    }
 }
