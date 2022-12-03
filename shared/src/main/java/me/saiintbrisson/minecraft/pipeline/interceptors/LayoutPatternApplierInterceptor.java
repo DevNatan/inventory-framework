44c5c7bf3b3dbbe4246e9668f558098957c46eef
Natan Vieira <natanvnascimento@gmail.com>
2022-12-03 18:37:00 -0300

    Fix iteration index and item mutability

--- shared/src/main/java/me/saiintbrisson/minecraft/pipeline/interceptors/LayoutPatternApplierInterceptor.java
+++ shared/src/main/java/me/saiintbrisson/minecraft/pipeline/interceptors/LayoutPatternApplierInterceptor.java
@@ -1,10 +1,8 @@
 package me.saiintbrisson.minecraft.pipeline.interceptors;
 
 import java.util.List;
-import java.util.function.Consumer;
-import me.saiintbrisson.minecraft.AbstractView;
+import java.util.function.BiConsumer;
 import me.saiintbrisson.minecraft.LayoutPattern;
-import me.saiintbrisson.minecraft.ViewContext;
 import me.saiintbrisson.minecraft.ViewItem;
 import me.saiintbrisson.minecraft.VirtualView;
 import me.saiintbrisson.minecraft.pipeline.PipelineContext;
@@ -12,7 +10,7 @@ import me.saiintbrisson.minecraft.pipeline.PipelineInterceptor;
 import org.jetbrains.annotations.NotNull;
 
 /**
- * Applies items from {@link VirtualView#setLayout(char, Consumer)  user-defined layout patterns}
+ * Applies items from {@link VirtualView#setLayout(char, BiConsumer) user-defined layout patterns}
  * to be rendered later by {@link RenderInterceptor}.
  */
 public final class LayoutPatternApplierInterceptor implements PipelineInterceptor<VirtualView> {
@@ -20,16 +18,15 @@ public final class LayoutPatternApplierInterceptor implements PipelineIntercepto
     @Override
     public void intercept(@NotNull PipelineContext<VirtualView> pipeline, VirtualView view) {
         final List<LayoutPattern> patterns = view.getLayoutPatterns();
-        if (patterns == null || patterns.isEmpty()) return;
+        if (patterns == null) return;
 
-        final boolean applyOnRoot = view instanceof AbstractView;
-        final AbstractView root = applyOnRoot ? (AbstractView) view : ((ViewContext) view).getRoot();
-
-        int iterationIndex = 0;
         for (final LayoutPattern pattern : patterns) {
-            final ViewItem item = pattern.getFactory().apply(iterationIndex++);
+            int iterationIndex = 0;
 
-            for (final int slot : pattern.getSlots()) (applyOnRoot ? root : view).apply(item, slot);
+            for (final int slot : pattern.getSlots()) {
+                final ViewItem item = pattern.getFactory().apply(iterationIndex++);
+                view.apply(item, slot);
+            }
         }
     }
 }
