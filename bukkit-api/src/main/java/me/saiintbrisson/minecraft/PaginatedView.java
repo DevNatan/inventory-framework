ad4c2c9aff754d1d765ee52c4d038514eea25978
Natan Vieira <natanvnascimento@gmail.com>
2022-12-25 22:24:21 -0300

    Deprecate PaginatedView

--- bukkit-api/src/main/java/me/saiintbrisson/minecraft/PaginatedView.java
+++ bukkit-api/src/main/java/me/saiintbrisson/minecraft/PaginatedView.java
@@ -1,3 +1,7 @@
 package me.saiintbrisson.minecraft;
 
+/**
+ * @deprecated Deprecated in favor of new <a href="https://github.com/DevNatan/inventory-framework/wiki/Pagination">State Management System</a>.
+ */
+@Deprecated
 public abstract class PaginatedView<T> extends AbstractPaginatedView<T> {}
