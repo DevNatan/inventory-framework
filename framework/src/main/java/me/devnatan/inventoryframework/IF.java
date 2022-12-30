489a5f89d34b29df91641b94bef72f1f98e1653b
Natan Vieira <natanvnascimento@gmail.com>
2022-12-30 12:46:53 -0300

    Update version

--- framework/src/main/java/me/devnatan/inventoryframework/IF.java
+++ framework/src/main/java/me/devnatan/inventoryframework/IF.java
@@ -6,5 +6,5 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public final class IF {
 
-    public static final String VERSION = "2.5.4-rc.1";
+    public static final String VERSION = "3.0.0";
 }
