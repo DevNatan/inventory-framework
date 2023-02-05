71f2f1aa10cb4322a0e8504925fc94be4a544bba
Natan Vieira <natanvnascimento@gmail.com>
2023-02-05 12:32:20 -0300

    Documentation to StandardPipelinePhases

--- api/src/main/java/me/devnatan/inventoryframework/pipeline/StandardPipelinePhases.java
+++ api/src/main/java/me/devnatan/inventoryframework/pipeline/StandardPipelinePhases.java
@@ -2,17 +2,48 @@ package me.devnatan.inventoryframework.pipeline;
 
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
+import me.devnatan.inventoryframework.RootView;
+import me.devnatan.inventoryframework.context.IFContext;
 
+/**
+ * Pipeline phases used internally by the framework. All phases have a {@link IFContext} as subject.
+ */
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public final class StandardPipelinePhases {
 
-    public static final PipelinePhase OPEN = new PipelinePhase("open"),
-            INIT = new PipelinePhase("init"),
-            RENDER = new PipelinePhase("render"),
-            SLOT_RENDER = new PipelinePhase("slot-render"),
-            PAGINATED_ITEM_RENDER = new PipelinePhase("paginated-item-render"),
-            RESUME = new PipelinePhase("resume"),
-            UPDATE = new PipelinePhase("update"),
-            CLICK = new PipelinePhase("click"),
-            CLOSE = new PipelinePhase("close");
+	/**
+	 * Called when a {@link RootView root} is initialized for the first time.
+	 */
+	public static final PipelinePhase INIT = new PipelinePhase("init");
+
+	/**
+	 * Called when a context is about to open.
+	 */
+	public static final PipelinePhase OPEN = new PipelinePhase("open");
+
+	/**
+	 * Called when a {@link IFContext context} is going to be rendered.
+	 */
+	public static final PipelinePhase RENDER = new PipelinePhase("render");
+
+	/**
+	 * Called when a {@link IFContext context} is going to be updated.
+	 */
+	public static final PipelinePhase UPDATE = new PipelinePhase("update");
+
+	/**
+	 * Called when a click is detected in a context.
+	 */
+	public static final PipelinePhase CLICK = new PipelinePhase("click");
+
+	/**
+	 * Called when a context is closed for a viewer. This phase is called several times in shared
+	 * contexts and its complete closure can be determined by the number of viewers.
+	 */
+	public static final PipelinePhase CLOSE = new PipelinePhase("close");
+
+	/**
+	 * A context is invalidated, or considered invalidated, when there are no more viewers in it.
+	 */
+	public static final PipelinePhase INVALIDATION = new PipelinePhase("invalidation");
 }
