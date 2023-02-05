49cb9b81548884be7a06a7b9133253da0193299c
Natan Vieira <natanvnascimento@gmail.com>
2023-02-05 12:34:14 -0300

    Documentation to IFCloseContext

--- api/src/main/java/me/devnatan/inventoryframework/context/IFCloseContext.java
+++ api/src/main/java/me/devnatan/inventoryframework/context/IFCloseContext.java
@@ -1,4 +1,22 @@
 package me.devnatan.inventoryframework.context;
 
 public interface IFCloseContext extends IFContext {
+
+	/**
+	 * Cancellation state of that context.
+	 * <p>
+	 *
+	 * @return If that context was cancelled.
+	 */
+	boolean isCancelled();
+
+	/**
+	 * Cancels or not this context.
+	 * <p>
+	 * The side effect of canceling a close context is determined by the current platform.
+	 *
+	 * @param cancelled If this context should be cancelled.
+	 */
+	void setCancelled(boolean cancelled);
+
 }
