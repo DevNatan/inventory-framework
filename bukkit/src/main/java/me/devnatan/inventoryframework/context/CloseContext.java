8d48ed6393279473d1c02e94785ba18abb9e3729
Natan Vieira <natanvnascimento@gmail.com>
2023-02-05 12:40:57 -0300

    Add missing cancelled field on CloseContext

--- bukkit/src/main/java/me/devnatan/inventoryframework/context/CloseContext.java
+++ bukkit/src/main/java/me/devnatan/inventoryframework/context/CloseContext.java
@@ -10,6 +10,7 @@ import org.jetbrains.annotations.NotNull;
 public final class CloseContext extends ConfinedContext implements IFCloseContext, ViewContext {
 
 	private final Player player;
+	private boolean cancelled;
 
 	public CloseContext(@NotNull RootView root, @NotNull ViewContainer container, @NotNull Viewer viewer) {
 		super(root, container, viewer);
@@ -30,4 +31,15 @@ public final class CloseContext extends ConfinedContext implements IFCloseContex
 	public @NotNull Player getPlayer() {
 		return player;
 	}
+
+	@Override
+	public boolean isCancelled() {
+		return cancelled;
+	}
+
+	@Override
+	public void setCancelled(boolean cancelled) {
+		this.cancelled = cancelled;
+	}
+
 }
