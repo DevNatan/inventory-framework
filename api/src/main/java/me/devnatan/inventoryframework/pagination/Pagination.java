f46fb26287cf09d7c8f9d912aa60ccfe20d5ddfd
Natan Vieira <natanvnascimento@gmail.com>
2023-02-12 14:42:39 -0300

    Pagination as component composition

--- api/src/main/java/me/devnatan/inventoryframework/pagination/Pagination.java
+++ api/src/main/java/me/devnatan/inventoryframework/pagination/Pagination.java
@@ -1,71 +1,95 @@
 package me.devnatan.inventoryframework.pagination;
 
+import lombok.RequiredArgsConstructor;
+import me.devnatan.inventoryframework.RootView;
+import me.devnatan.inventoryframework.VirtualView;
+import me.devnatan.inventoryframework.component.Component;
+import me.devnatan.inventoryframework.component.ComponentComposition;
+import me.devnatan.inventoryframework.context.IFContext;
 import me.devnatan.inventoryframework.state.State;
 import me.devnatan.inventoryframework.state.StateHolder;
 import org.jetbrains.annotations.NotNull;
 
-public final class Pagination implements State<Pagination> {
+@RequiredArgsConstructor
+public final class Pagination implements State<Pagination>, ComponentComposition {
 
-    /**
-     * The index-based current page number.
-     *
-     * @param holder The holder whose value will be obtained from.
-     * @return The current page number. {@code 0} will be the first page.
-     */
-    public int getCurrentPage(@NotNull StateHolder holder) {
-        throw new UnsupportedOperationException();
-    }
+	private final RootView root;
 
-    /**
-     * Checks for pages before the {@link #getCurrentPage(StateHolder) current} one.
-     *
-     * @param holder The holder whose value will be obtained from.
-     * @return {@code true} if there are previous pages or {@code false} otherwise
-     */
-    public boolean hasPreviousPage(@NotNull StateHolder holder) {
-        throw new UnsupportedOperationException();
-    }
+	@Override
+	public @NotNull VirtualView getRoot() {
+		return root;
+	}
 
-    /**
-     * Checks for pages after the {@link #getCurrentPage(StateHolder) current} one.
-     *
-     * @param holder The holder whose value will be obtained from.
-     * @return {@code true} if there are next pages or {@code false} otherwise
-     */
-    public boolean hasNextPage(@NotNull StateHolder holder) {
-        throw new UnsupportedOperationException();
-    }
+	@Override
+	public int getPosition() {
+		return 0;
+	}
 
-    public boolean isFirstPage(@NotNull StateHolder holder) {
-        throw new UnsupportedOperationException();
-    }
+	@Override
+	public Component[] getComponents() {
+		return new Component[0];
+	}
 
-    public boolean isLastPage(@NotNull StateHolder holder) {
-        throw new UnsupportedOperationException();
-    }
+	/**
+	 * The index-based current page number.
+	 *
+	 * @param context The context whose value will be obtained from.
+	 * @return The current page number. {@code 0} will be the first page.
+	 */
+	public int getCurrentPage(@NotNull IFContext context) {
+		throw new UnsupportedOperationException();
+	}
 
-    public int count(@NotNull StateHolder holder) {
-        throw new UnsupportedOperationException();
-    }
+	/**
+	 * Checks for pages before the current page.
+	 *
+	 * @param context The context whose value will be obtained from.
+	 * @return {@code true} if there are previous pages or {@code false} otherwise
+	 */
+	public boolean hasPreviousPage(@NotNull IFContext context) {
+		throw new UnsupportedOperationException();
+	}
 
-    public void back(@NotNull StateHolder holder) {
-        throw new UnsupportedOperationException();
-    }
+	/**
+	 * Checks for pages after the current page.
+	 *
+	 * @param context The context whose value will be obtained from.
+	 * @return {@code true} if there are next pages or {@code false} otherwise
+	 */
+	public boolean hasNextPage(@NotNull IFContext context) {
+		throw new UnsupportedOperationException();
+	}
 
-    public void advance(@NotNull StateHolder holder) {
-        throw new UnsupportedOperationException();
-    }
+	public boolean isFirstPage(@NotNull StateHolder holder) {
+		throw new UnsupportedOperationException();
+	}
 
-    public boolean canBack(@NotNull StateHolder holder) {
-        throw new UnsupportedOperationException();
-    }
+	public boolean isLastPage(@NotNull StateHolder holder) {
+		throw new UnsupportedOperationException();
+	}
 
-    public boolean canAdvance(@NotNull StateHolder holder) {
-        throw new UnsupportedOperationException();
-    }
+	public int count(@NotNull StateHolder holder) {
+		throw new UnsupportedOperationException();
+	}
 
-    @Override
-    public Pagination get(@NotNull StateHolder holder) {
-        throw new UnsupportedOperationException();
-    }
+	public void back(@NotNull StateHolder holder) {
+		throw new UnsupportedOperationException();
+	}
+
+	public void advance(@NotNull StateHolder holder) {
+		throw new UnsupportedOperationException();
+	}
+
+	public boolean canBack(@NotNull StateHolder holder) {
+		throw new UnsupportedOperationException();
+	}
+
+	public boolean canAdvance(@NotNull StateHolder holder) {
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public Pagination get(@NotNull StateHolder holder) {
+		throw new UnsupportedOperationException();
+	}
 }
