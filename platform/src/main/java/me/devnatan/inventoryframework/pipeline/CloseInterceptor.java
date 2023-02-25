4fddbf61816460cf5f409d2e4cbd6e9f23529e72
Natan Vieira <natanvnascimento@gmail.com>
2023-02-25 13:17:19 -0300

    Handle viewer and context removal on close

--- platform/src/main/java/me/devnatan/inventoryframework/pipeline/CloseInterceptor.java
+++ platform/src/main/java/me/devnatan/inventoryframework/pipeline/CloseInterceptor.java
@@ -1,12 +1,54 @@
 package me.devnatan.inventoryframework.pipeline;
 
+import me.devnatan.inventoryframework.PlatformView;
+import me.devnatan.inventoryframework.RootView;
+import me.devnatan.inventoryframework.Viewer;
+import me.devnatan.inventoryframework.context.IFCloseContext;
 import me.devnatan.inventoryframework.context.IFContext;
 import org.jetbrains.annotations.NotNull;
 
-public final class CloseInterceptor implements PipelineInterceptor<IFContext> {
+public final class CloseInterceptor implements PipelineInterceptor<IFCloseContext> {
 
     @Override
-    public void intercept(@NotNull PipelineContext<IFContext> pipeline, IFContext context) {
-        throw new UnsupportedOperationException();
+    public void intercept(@NotNull PipelineContext<IFCloseContext> pipeline, IFCloseContext context) {
+        final RootView root = context.getRoot();
+        tryCallPlatformRootCloseHandler(root, context);
+
+        final Viewer viewer = context.getViewer();
+        if (context.isCancelled()) {
+            // prevent platform-specific post-close handling since context was cancelled
+            pipeline.finish();
+            return;
+        }
+
+        if (canContextBeInvalidated(context)) {
+            // TODO invalidate context
+            root.removeContext(context);
+        }
+
+        context.removeViewer(viewer);
+    }
+
+    /**
+     * Calls {@link PlatformView}'s close handler if <code>root</code> is a PlatformView.
+     *
+     * @param root    The platform root view.
+     * @param context The subject context for the close handler.
+     */
+    @SuppressWarnings({"unchecked", "rawtypes"})
+    private void tryCallPlatformRootCloseHandler(RootView root, IFCloseContext context) {
+        if (root instanceof PlatformView) {
+            ((PlatformView) root).onClose(context);
+        }
+    }
+
+    /**
+     * Checks if a context can be invalidated by checking its viewers count.
+     *
+     * @param context The subject context.
+     * @return If context can be invalidated.
+     */
+    private boolean canContextBeInvalidated(IFContext context) {
+        return context.getViewers().size() == 1 /* last one is the current actor */;
     }
 }
