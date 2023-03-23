eae7318707e480e07ede684651ac5e1ca5974cfc
Natan Vieira <natanvnascimento@gmail.com>
2023-03-23 14:07:06 -0300

    Create base platform render context

--- bukkit/src/main/java/me/devnatan/inventoryframework/context/RenderContext.java
+++ bukkit/src/main/java/me/devnatan/inventoryframework/context/RenderContext.java
@@ -1,65 +1,32 @@
 package me.devnatan.inventoryframework.context;
 
-import static java.lang.String.format;
-import static me.devnatan.inventoryframework.utils.SlotConverter.convertSlot;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
-import java.util.UUID;
-import java.util.function.BiConsumer;
-import java.util.function.BiFunction;
 import lombok.Getter;
 import me.devnatan.inventoryframework.RootView;
 import me.devnatan.inventoryframework.ViewContainer;
 import me.devnatan.inventoryframework.Viewer;
 import me.devnatan.inventoryframework.bukkit.BukkitViewer;
 import me.devnatan.inventoryframework.component.BukkitItemComponentBuilder;
-import me.devnatan.inventoryframework.component.ComponentFactory;
 import me.devnatan.inventoryframework.internal.LayoutSlot;
-import me.devnatan.inventoryframework.state.StateHost;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 import org.jetbrains.annotations.ApiStatus;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
-import org.jetbrains.annotations.UnmodifiableView;
 
 @Getter
-public final class RenderContext extends ConfinedContext implements IFRenderContext, Context {
+public final class RenderContext extends PlatformRenderContext<BukkitItemComponentBuilder> implements Context {
 
     private static final LayoutSlot filledReservedCharLayoutSlot =
             new LayoutSlot(LayoutSlot.FILLED_RESERVED_CHAR, $ -> {
                 throw new IllegalStateException("Cannot use factory of reserved char");
             });
 
-    private final @NotNull IFContext parent;
     private final @NotNull Player player;
 
-    private final List<ComponentFactory> componentBuilders = new ArrayList<>();
-    private final List<LayoutSlot> layoutSlots = new ArrayList<>(Arrays.asList(filledReservedCharLayoutSlot));
-    private final List<BiFunction<Integer, Integer, ComponentFactory>> availableSlots = new ArrayList<>();
-
     @ApiStatus.Internal
-    public RenderContext(
-            @NotNull RootView root,
-            @NotNull ViewContainer container,
-            @NotNull Viewer viewer,
-            @NotNull IFContext parent) {
-        super(root, container, viewer);
+    public RenderContext(RootView root, ViewContainer container, Viewer viewer, @NotNull IFContext parent) {
+        super(root, container, viewer, parent);
         this.player = ((BukkitViewer) viewer).getPlayer();
-        this.parent = parent;
-    }
-
-    /**
-     * Adds an item to a specific slot in the context container.
-     *
-     * @param slot The slot in which the item will be positioned.
-     * @return An item builder to configure the item.
-     */
-    public @NotNull BukkitItemComponentBuilder slot(int slot) {
-        return createRegisteredComponentBuilder().withSlot(slot);
     }
 
     /**
@@ -69,24 +36,7 @@ public final class RenderContext extends ConfinedContext implements IFRenderCont
      * @return An item builder to configure the item.
      */
     public @NotNull BukkitItemComponentBuilder slot(int slot, @Nullable ItemStack item) {
-        return createRegisteredComponentBuilder().withSlot(slot).withItem(item);
-    }
-
-    /**
-     * Adds an item at the specific column and ROW (X, Y) in that context's container.
-     *
-     * @param row    The row (Y) in which the item will be positioned.
-     * @param column The column (X) in which the item will be positioned.
-     * @return An item builder to configure the item.
-     */
-    @NotNull
-    public BukkitItemComponentBuilder slot(int row, int column) {
-        return createRegisteredComponentBuilder()
-                .withSlot(convertSlot(
-                        row,
-                        column,
-                        getContainer().getRowsCount(),
-                        getContainer().getColumnsCount()));
+        return slot(slot).withItem(item);
     }
 
     /**
@@ -98,22 +48,7 @@ public final class RenderContext extends ConfinedContext implements IFRenderCont
      */
     @NotNull
     public BukkitItemComponentBuilder slot(int row, int column, @Nullable ItemStack item) {
-        return createRegisteredComponentBuilder()
-                .withSlot(convertSlot(
-                        row,
-                        column,
-                        getContainer().getRowsCount(),
-                        getContainer().getColumnsCount()))
-                .withItem(item);
-    }
-
-    /**
-     * Sets an item in the first slot of this context's container.
-     *
-     * @return An item builder to configure the item.
-     */
-    public @NotNull BukkitItemComponentBuilder firstSlot() {
-        return createRegisteredComponentBuilder().withSlot(getContainer().getFirstSlot());
+        return slot(row, column).withItem(item);
     }
 
     /**
@@ -123,18 +58,7 @@ public final class RenderContext extends ConfinedContext implements IFRenderCont
      * @return An item builder to configure the item.
      */
     public @NotNull BukkitItemComponentBuilder firstSlot(@Nullable ItemStack item) {
-        return createRegisteredComponentBuilder()
-                .withSlot(getContainer().getFirstSlot())
-                .withItem(item);
-    }
-
-    /**
-     * Sets an item in the last slot of this context's container.
-     *
-     * @return An item builder to configure the item.
-     */
-    public @NotNull BukkitItemComponentBuilder lastSlot() {
-        return createRegisteredComponentBuilder().withSlot(getContainer().getLastSlot());
+        return firstSlot().withItem(item);
     }
 
     /**
@@ -144,20 +68,7 @@ public final class RenderContext extends ConfinedContext implements IFRenderCont
      * @return An item builder to configure the item.
      */
     public @NotNull BukkitItemComponentBuilder lastSlot(@Nullable ItemStack item) {
-        return createRegisteredComponentBuilder()
-                .withSlot(getContainer().getLastSlot())
-                .withItem(item);
-    }
-
-    /**
-     * Adds an item in the next available slot of this context's container.
-     *
-     * @return An item builder to configure the item.
-     */
-    public @NotNull BukkitItemComponentBuilder availableSlot() {
-        final BukkitItemComponentBuilder builder = new BukkitItemComponentBuilder();
-        availableSlots.add((index, slot) -> builder.withSlot(slot));
-        return builder;
+        return lastSlot().withItem(item);
     }
 
     /**
@@ -171,35 +82,6 @@ public final class RenderContext extends ConfinedContext implements IFRenderCont
     }
 
     /**
-     * Adds an item in the next available slot of this context's container.
-     *
-     * @param factory A factory to create the item builder to configure the item.
-     *                The first parameter is the iteration index of the available slot.
-     */
-    public void availableSlot(@NotNull BiConsumer<Integer, BukkitItemComponentBuilder> factory) {
-        final BukkitItemComponentBuilder builder = new BukkitItemComponentBuilder();
-        availableSlots.add((index, slot) -> {
-            builder.withSlot(slot);
-            factory.accept(index, builder);
-            return builder;
-        });
-    }
-
-    /**
-     * Defines the item that will represent a character provided in the context layout.
-     *
-     * @param character The layout character target.
-     * @return An item builder to configure the item.
-     */
-    public @NotNull BukkitItemComponentBuilder layoutSlot(char character) {
-        requireNonReservedLayoutCharacter(character);
-
-        final BukkitItemComponentBuilder builder = new BukkitItemComponentBuilder();
-        layoutSlots.add(new LayoutSlot(character, $ -> builder));
-        return builder;
-    }
-
-    /**
      * Defines the item that will represent a character provided in the context layout.
      *
      * @param character The layout character target.
@@ -207,69 +89,11 @@ public final class RenderContext extends ConfinedContext implements IFRenderCont
      * @return An item builder to configure the item.
      */
     public @NotNull BukkitItemComponentBuilder layoutSlot(char character, @Nullable ItemStack item) {
-        requireNonReservedLayoutCharacter(character);
-
-        final BukkitItemComponentBuilder builder = new BukkitItemComponentBuilder().withItem(item);
-        layoutSlots.add(new LayoutSlot(character, $ -> builder));
-        return builder;
-    }
-
-    /**
-     * Defines the item that will represent a character provided in the context layout.
-     *
-     * @param character The layout character target.
-     */
-    public void layoutSlot(char character, @NotNull BiConsumer<Integer, BukkitItemComponentBuilder> factory) {
-        requireNonReservedLayoutCharacter(character);
-
-        layoutSlots.add(new LayoutSlot(character, index -> {
-            final BukkitItemComponentBuilder builder = new BukkitItemComponentBuilder();
-            factory.accept(index, builder);
-            return builder;
-        }));
-    }
-
-    private void requireNonReservedLayoutCharacter(char character) {
-        if (character == LayoutSlot.FILLED_RESERVED_CHAR)
-            throw new IllegalArgumentException(format(
-                    "The '%c' character cannot be used because it is only available for backwards compatibility. Please use another character.",
-                    character));
-    }
-
-    /**
-     * Creates a BukkitItemBuilder instance and registers it in this context.
-     *
-     * @return A new registered BukkitItemBuilder instance.
-     */
-    private BukkitItemComponentBuilder createRegisteredComponentBuilder() {
-        final BukkitItemComponentBuilder builder = new BukkitItemComponentBuilder();
-        componentBuilders.add(builder);
-        return builder;
+        return layoutSlot(character).withItem(item);
     }
 
     @Override
-    public @NotNull UUID getId() {
-        return getParent().getId();
-    }
-
-    @Override
-    public @NotNull StateHost getStateHost() {
-        return getParent().getStateHost();
-    }
-
-    @Override
-    public @NotNull @UnmodifiableView List<ComponentFactory> getComponentFactories() {
-        return Collections.unmodifiableList(componentBuilders);
-    }
-
-    @Override
-    public @NotNull @UnmodifiableView List<LayoutSlot> getLayoutSlots() {
-        return Collections.unmodifiableList(layoutSlots);
-    }
-
-    @Override
-    public @NotNull @UnmodifiableView List<BiFunction<Integer, Integer, ComponentFactory>>
-            getAvailableSlotsFactories() {
-        return Collections.unmodifiableList(availableSlots);
+    protected BukkitItemComponentBuilder createBuilder() {
+        return new BukkitItemComponentBuilder();
     }
 }
---
+++ platform/src/main/java/me/devnatan/inventoryframework/context/PlatformRenderContext.java
@@ -0,0 +1,200 @@
+package me.devnatan.inventoryframework.context;
+
+import static java.lang.String.format;
+import static me.devnatan.inventoryframework.utils.SlotConverter.convertSlot;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.UUID;
+import java.util.function.BiConsumer;
+import java.util.function.BiFunction;
+import lombok.Getter;
+import me.devnatan.inventoryframework.RootView;
+import me.devnatan.inventoryframework.ViewContainer;
+import me.devnatan.inventoryframework.Viewer;
+import me.devnatan.inventoryframework.component.ComponentFactory;
+import me.devnatan.inventoryframework.component.ItemComponentBuilder;
+import me.devnatan.inventoryframework.internal.LayoutSlot;
+import me.devnatan.inventoryframework.state.StateHost;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.UnmodifiableView;
+
+abstract class PlatformRenderContext<T extends ItemComponentBuilder<T>> extends ConfinedContext
+        implements IFRenderContext {
+
+    private static final LayoutSlot filledReservedCharLayoutSlot =
+            new LayoutSlot(LayoutSlot.FILLED_RESERVED_CHAR, $ -> {
+                throw new IllegalStateException("Cannot use factory of reserved char");
+            });
+
+    @Getter
+    private final @NotNull IFContext parent;
+
+    private final List<ComponentFactory> componentBuilders = new ArrayList<>();
+    private final List<LayoutSlot> layoutSlots =
+            new ArrayList<>(Collections.singletonList(filledReservedCharLayoutSlot));
+    private final List<BiFunction<Integer, Integer, ComponentFactory>> availableSlots = new ArrayList<>();
+
+    PlatformRenderContext(RootView root, ViewContainer container, Viewer viewer, @NotNull IFContext parent) {
+        super(root, container, viewer);
+        this.parent = parent;
+    }
+
+    /**
+     * Adds an item to a specific slot in the context container.
+     *
+     * @param slot The slot in which the item will be positioned.
+     * @return An item builder to configure the item.
+     */
+    public final @NotNull T slot(int slot) {
+        return createRegisteredBuilder().withSlot(slot);
+    }
+
+    /**
+     * Adds an item at the specific column and ROW (X, Y) in that context's container.
+     *
+     * @param row    The row (Y) in which the item will be positioned.
+     * @param column The column (X) in which the item will be positioned.
+     * @return An item builder to configure the item.
+     */
+    @NotNull
+    public T slot(int row, int column) {
+        return createRegisteredBuilder()
+                .withSlot(convertSlot(
+                        row,
+                        column,
+                        getContainer().getRowsCount(),
+                        getContainer().getColumnsCount()));
+    }
+
+    /**
+     * Sets an item in the first slot of this context's container.
+     *
+     * @return An item builder to configure the item.
+     */
+    public @NotNull T firstSlot() {
+        return createRegisteredBuilder().withSlot(getContainer().getFirstSlot());
+    }
+
+    /**
+     * Sets an item in the last slot of this context's container.
+     *
+     * @return An item builder to configure the item.
+     */
+    public @NotNull T lastSlot() {
+        return createRegisteredBuilder().withSlot(getContainer().getLastSlot());
+    }
+
+    /**
+     * Adds an item in the next available slot of this context's container.
+     *
+     * @return An item builder to configure the item.
+     */
+    public @NotNull T availableSlot() {
+        final T builder = createBuilder();
+        availableSlots.add((index, slot) -> (ComponentFactory) builder.withSlot(slot));
+        return builder;
+    }
+
+    /**
+     * Adds an item in the next available slot of this context's container.
+     *
+     * @param factory A factory to create the item builder to configure the item.
+     *                The first parameter is the iteration index of the available slot.
+     */
+    public void availableSlot(@NotNull BiConsumer<Integer, T> factory) {
+        final T builder = createBuilder();
+        availableSlots.add((index, slot) -> {
+            builder.withSlot(slot);
+            factory.accept(index, builder);
+            return (ComponentFactory) builder;
+        });
+    }
+
+    /**
+     * Defines the item that will represent a character provided in the context layout.
+     *
+     * @param character The layout character target.
+     * @return An item builder to configure the item.
+     */
+    public @NotNull T layoutSlot(char character) {
+        requireNonReservedLayoutCharacter(character);
+
+        final T builder = createBuilder();
+        layoutSlots.add(new LayoutSlot(character, $ -> (ComponentFactory) builder));
+        return builder;
+    }
+
+    /**
+     * Defines the item that will represent a character provided in the context layout.
+     *
+     * @param character The layout character target.
+     */
+    public void layoutSlot(char character, @NotNull BiConsumer<Integer, T> factory) {
+        requireNonReservedLayoutCharacter(character);
+
+        layoutSlots.add(new LayoutSlot(character, index -> {
+            final T builder = createBuilder();
+            factory.accept(index, builder);
+            return (ComponentFactory) builder;
+        }));
+    }
+
+    /**
+     * Checks if the character is a reserved layout character.
+     *
+     * @param character The character to be checked.re
+     * @throws IllegalArgumentException If the given character is a reserved layout character.
+     */
+    private void requireNonReservedLayoutCharacter(char character) {
+        if (character == LayoutSlot.FILLED_RESERVED_CHAR)
+            throw new IllegalArgumentException(format(
+                    "The '%c' character cannot be used because it is only available for backwards compatibility. Please use another character.",
+                    character));
+    }
+
+    /**
+     * Creates a new platform builder instance.
+     *
+     * @return A new platform builder instance.
+     */
+    protected abstract T createBuilder();
+
+    /**
+     * Creates a new platform builder instance and registers it.
+     *
+     * @return A new registered platform builder instance.
+     */
+    protected final T createRegisteredBuilder() {
+        final T builder = createBuilder();
+        componentBuilders.add((ComponentFactory) builder);
+        return builder;
+    }
+
+    @Override
+    public @NotNull UUID getId() {
+        return getParent().getId();
+    }
+
+    @Override
+    public @NotNull StateHost getStateHost() {
+        return getParent().getStateHost();
+    }
+
+    @Override
+    public final @NotNull @UnmodifiableView List<ComponentFactory> getComponentFactories() {
+        return Collections.unmodifiableList(componentBuilders);
+    }
+
+    @Override
+    public final @NotNull @UnmodifiableView List<LayoutSlot> getLayoutSlots() {
+        return Collections.unmodifiableList(layoutSlots);
+    }
+
+    @Override
+    public final @NotNull @UnmodifiableView List<BiFunction<Integer, Integer, ComponentFactory>>
+            getAvailableSlotsFactories() {
+        return Collections.unmodifiableList(availableSlots);
+    }
+}
