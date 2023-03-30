dccf9483d610017fb7b33e637010b2e495796f40
Natan Vieira <natanvnascimento@gmail.com>
2023-03-29 21:30:07 -0300

    Delete examples

--- examples/src/main/java/me/devnatan/inventoryframework/example/tournament/TournamentsManager.java
+++
@@ -1,9 +0,0 @@
-package me.devnatan.inventoryframework.example.tournament;
-
-import java.util.UUID;
-
-public interface TournamentsManager {
-    Tournament getTournament(UUID id);
-
-    void leaveTournament(UUID tournamentId, UUID participantId);
-}
--- examples/src/main/java/me/devnatan/inventoryframework/example/warpList/WarpsManager.java
+++
@@ -1,8 +0,0 @@
-package me.devnatan.inventoryframework.example.warpList;
-
-import java.util.List;
-
-public interface WarpsManager {
-
-    List<Warp> getWarps();
-}
