022bdaa09cef6b419e087831543436c97aca4f96
Natan Vieira <natanvnascimento@gmail.com>
2023-04-03 20:25:16 -0300

    Lint codebase

--- bukkit/src/main/java/me/devnatan/inventoryframework/runtime/InventoryFramework.java
+++ bukkit/src/main/java/me/devnatan/inventoryframework/runtime/InventoryFramework.java
@@ -3,5 +3,4 @@ package me.devnatan.inventoryframework.runtime;
 import org.bukkit.plugin.java.JavaPlugin;
 
 @SuppressWarnings("unused")
-public final class InventoryFramework extends JavaPlugin {
-}
+public final class InventoryFramework extends JavaPlugin {}
