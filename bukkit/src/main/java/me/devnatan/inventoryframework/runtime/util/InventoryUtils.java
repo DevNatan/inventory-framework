c50b2e5a6bcf807a54585f340868a20c29ef3fcc
Natan Vieira <natanvnascimento@gmail.com>
2023-04-08 17:35:44 -0300

    Support more inventory types

--- bukkit/src/main/java/me/devnatan/inventoryframework/runtime/util/InventoryUtils.java
+++ bukkit/src/main/java/me/devnatan/inventoryframework/runtime/util/InventoryUtils.java
@@ -1,5 +1,7 @@
 package me.devnatan.inventoryframework.runtime.util;
 
+import com.google.common.collect.ImmutableMap;
+import java.util.Map;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 import me.devnatan.inventoryframework.ViewType;
@@ -9,13 +11,24 @@ import org.jetbrains.annotations.NotNull;
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public final class InventoryUtils {
 
-    public static InventoryType toInventoryType(@NotNull ViewType type) {
-        if (type == ViewType.HOPPER) return InventoryType.HOPPER;
-        if (type == ViewType.FURNACE) return InventoryType.FURNACE;
-        if (type == ViewType.CHEST) return InventoryType.CHEST;
-        if (type == ViewType.CRAFTING_TABLE) return InventoryType.WORKBENCH;
+    private static final Map<ViewType, InventoryType> typeMappings = ImmutableMap.<ViewType, InventoryType>builder()
+            .put(ViewType.CHEST, InventoryType.CHEST)
+            .put(ViewType.HOPPER, InventoryType.HOPPER)
+            .put(ViewType.DROPPER, InventoryType.DROPPER)
+            .put(ViewType.DISPENSER, InventoryType.DISPENSER)
+            .put(ViewType.FURNACE, InventoryType.FURNACE)
+            .put(ViewType.BLAST_FURNACE, InventoryType.BLAST_FURNACE)
+            .put(ViewType.CRAFTING_TABLE, InventoryType.DISPENSER)
+            .put(ViewType.BREWING_STAND, InventoryType.BREWING)
+            .put(ViewType.BEACON, InventoryType.BEACON)
+            .put(ViewType.ANVIL, InventoryType.ANVIL)
+            .put(ViewType.SHULKER_BOX, InventoryType.SHULKER_BOX)
+            .put(ViewType.SMOKER, InventoryType.SMOKER)
+            .put(ViewType.VILLAGER_TRADING, InventoryType.MERCHANT)
+            .build();
 
-        return null;
+    public static InventoryType toInventoryType(@NotNull ViewType type) {
+        return typeMappings.get(type);
     }
 
     public static void checkInventoryTypeSupport(@NotNull ViewType type) {
