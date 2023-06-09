4512bd9bb077570415a6d857849ee27f0e9e72ba
Natan Vieira <natanvnascimento@gmail.com>
2023-06-09 18:13:56 -0300

    Fix compatibility issues

--- bukkit/src/main/java/me/devnatan/inventoryframework/internal/InventoryFactory.java
+++ bukkit/src/main/java/me/devnatan/inventoryframework/internal/InventoryFactory.java
@@ -1,36 +1,29 @@
 package me.devnatan.inventoryframework.internal;
 
-import static java.util.Objects.requireNonNull;
-import static me.devnatan.inventoryframework.runtime.util.InventoryUtils.toInventoryType;
-
 import me.devnatan.inventoryframework.ViewType;
-import org.bukkit.Bukkit;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.InventoryHolder;
 
-class InventoryFactory {
+abstract class InventoryFactory {
 
-    public Inventory createInventory(InventoryHolder holder, ViewType type, int size, Object title) {
-        return createInventoryWithTextTitle(holder, type, size, (String) title);
+    protected static InventoryFactory instance;
+
+    public static InventoryFactory current() {
+        if (instance == null) instance = forCurrentPlatform();
+        return instance;
     }
 
-    @SuppressWarnings("deprecation")
-    protected final Inventory createInventoryWithTextTitle(
-            InventoryHolder holder, ViewType type, int size, String title) {
-        final Inventory inventory;
-        title = title == null || title.isEmpty() ? null : title;
+    private static InventoryFactory forCurrentPlatform() {
+        try {
+            Class.forName("com.destroystokyo.paper.ParticleBuilder");
 
-        if (title == null) {
-            inventory = !type.isExtendable() || size == 0
-                    ? Bukkit.createInventory(holder, requireNonNull(toInventoryType(type)))
-                    : Bukkit.createInventory(holder, size);
-        } else if (!type.isExtendable()) {
-            inventory = Bukkit.createInventory(holder, requireNonNull(toInventoryType(type)), title);
-        } else {
-            inventory = size == 0
-                    ? Bukkit.createInventory(holder, requireNonNull(toInventoryType(type)), title)
-                    : Bukkit.createInventory(holder, size, title);
+            final Class<?> clazz = Class.forName("me.devnatan.inventoryframework.internal.PaperInventoryFramework");
+            return (InventoryFactory) clazz.newInstance();
+        } catch (final ClassNotFoundException | InstantiationException | IllegalAccessException ignored) {
         }
-        return inventory;
+
+        return new BukkitInventoryFactory();
     }
+
+    public abstract Inventory createInventory(InventoryHolder holder, ViewType type, int size, Object title);
 }
