ff00369ea5e216b9260b9d882357d4043899e670
Natan Vieira <natanvnascimento@gmail.com>
2023-07-16 18:55:57 -0300

    Update MutableState documentation

--- api/src/main/java/me/devnatan/inventoryframework/state/MutableState.java
+++ api/src/main/java/me/devnatan/inventoryframework/state/MutableState.java
@@ -3,17 +3,17 @@ package me.devnatan.inventoryframework.state;
 import org.jetbrains.annotations.NotNull;
 
 /**
- * Mutable variation of a state that exposes a function to set a new value for a state.
+ * Mutable variation of a {@link State} that the value can be changed.
  *
  * @param <T> The state value type.
  */
 public interface MutableState<T> extends State<T> {
 
-    /**
-     * Sets a new value for this state.
-     *
-     * @param value The new state value.
-     * @param host  The state host that'll catch the state change.
-     */
-    void set(T value, @NotNull StateValueHost host);
+	/**
+	 * Sets a new value for this state.
+	 *
+	 * @param value The new state value.
+	 * @param host  The state host that'll catch the state change.
+	 */
+	void set(T value, @NotNull StateValueHost host);
 }
