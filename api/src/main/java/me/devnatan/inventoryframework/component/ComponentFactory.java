8607ff8a31a73409cf9e886ff1585f80fe306d53
Natan Vieira <natanvnascimento@gmail.com>
2023-07-20 12:41:11 -0300

    Update javadocs

--- api/src/main/java/me/devnatan/inventoryframework/component/ComponentFactory.java
+++ api/src/main/java/me/devnatan/inventoryframework/component/ComponentFactory.java
@@ -2,8 +2,16 @@ package me.devnatan.inventoryframework.component;
 
 import org.jetbrains.annotations.NotNull;
 
+/**
+ * Factory for {@link Component} interface.
+ */
 public interface ComponentFactory {
 
+    /**
+     * Creates a new component.
+     *
+     * @return A new component instance.
+     */
     @NotNull
     Component create();
 }
--- api/src/main/java/me/devnatan/inventoryframework/exception/ContainerException.java
+++ api/src/main/java/me/devnatan/inventoryframework/exception/ContainerException.java
@@ -1,8 +1,13 @@
 package me.devnatan.inventoryframework.exception;
 
 import me.devnatan.inventoryframework.InventoryFrameworkException;
+import me.devnatan.inventoryframework.context.IFContext;
 
+/**
+ * Thrown when an error related to a {@link IFContext#getContainer() context container} occurs.
+ */
 public class ContainerException extends InventoryFrameworkException {
+
     public ContainerException(String message, Throwable cause) {
         super(message, cause);
     }
