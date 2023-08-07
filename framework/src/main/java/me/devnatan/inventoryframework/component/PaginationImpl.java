69974967ce4eb19bcaa846f2b8541bf72fbd6a0c
Natan Vieira do Nascimento <itroken99@gmail.com>
2023-08-07 19:01:13 -0300

    Remove wildcard import usage

--- framework/src/main/java/me/devnatan/inventoryframework/component/PaginationImpl.java
+++ framework/src/main/java/me/devnatan/inventoryframework/component/PaginationImpl.java
@@ -1,6 +1,14 @@
 package me.devnatan.inventoryframework.component;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.Set;
 import java.util.function.BiConsumer;
 import java.util.function.Function;
 import java.util.function.Supplier;
