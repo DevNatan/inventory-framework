2e2c6261d01d7498180996bc239b3bc999005492
Natan Vieira do Nascimento <itroken99@gmail.com>
2023-08-11 21:14:47 -0300

    Rename modules

--- api/src/main/java/me/devnatan/inventoryframework/exception/InvalidLayoutException.java
+++
@@ -1,9 +0,0 @@
-package me.devnatan.inventoryframework.exception;
-
-import me.devnatan.inventoryframework.InventoryFrameworkException;
-
-public final class InvalidLayoutException extends InventoryFrameworkException {
-    public InvalidLayoutException(String message) {
-        super(message);
-    }
-}
--- bukkit/src/main/java/me/devnatan/inventoryframework/runtime/InventoryFramework.java
+++
@@ -1,9 +0,0 @@
-package me.devnatan.inventoryframework.runtime;
-
-import org.bukkit.plugin.java.JavaPlugin;
-
-@SuppressWarnings("unused")
-public final class InventoryFramework extends JavaPlugin {
-
-    public static final String LIBRARY_VERSION = "3.0.0-rc.1";
-}
