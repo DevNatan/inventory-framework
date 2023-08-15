1ed1cf2771c0a6b18ae61dcedec14fd1e62217f1
Natan Vieira do Nascimento <itroken99@gmail.com>
2023-08-15 02:29:55 -0300

    Lint

--- inventory-framework-platform-bukkit/src/main/java/me/devnatan/inventoryframework/runtime/InventoryFramework.java
+++ inventory-framework-platform-bukkit/src/main/java/me/devnatan/inventoryframework/runtime/InventoryFramework.java
@@ -1,18 +1,5 @@
 package me.devnatan.inventoryframework.runtime;
 
-import java.util.concurrent.ThreadLocalRandom;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
-import me.devnatan.inventoryframework.View;
-import me.devnatan.inventoryframework.ViewConfigBuilder;
-import me.devnatan.inventoryframework.ViewFrame;
-import me.devnatan.inventoryframework.component.Pagination;
-import me.devnatan.inventoryframework.context.Context;
-import me.devnatan.inventoryframework.context.RenderContext;
-import me.devnatan.inventoryframework.state.State;
-import org.bukkit.Material;
-import org.bukkit.entity.Player;
-import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
 
 @SuppressWarnings("unused")
