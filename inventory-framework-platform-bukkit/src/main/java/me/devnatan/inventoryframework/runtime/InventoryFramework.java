074751e0d3ca5d9954e529fe0cd6442766e004ba
natan.nascimento <natanvnascimento@gmail.com>
2023-09-11 23:40:57 -0300

    Delete test code

--- inventory-framework-platform-bukkit/src/main/java/me/devnatan/inventoryframework/runtime/InventoryFramework.java
+++ inventory-framework-platform-bukkit/src/main/java/me/devnatan/inventoryframework/runtime/InventoryFramework.java
@@ -1,25 +1,8 @@
 package me.devnatan.inventoryframework.runtime;
 
-import me.devnatan.inventoryframework.View;
-import me.devnatan.inventoryframework.context.RenderContext;
-import me.devnatan.inventoryframework.state.History;
-import me.devnatan.inventoryframework.state.HistoryState;
-import org.bukkit.Material;
-import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class InventoryFramework extends JavaPlugin {
 
     public static final String LIBRARY_VERSION = "3.0.0-rc.2";
-}
-
-class TestView extends View {
-
-    private final HistoryState historyState = historyState();
-
-    @Override
-    public void onFirstRender(RenderContext render) {
-        final History history = historyState.get(render);
-        render.firstSlot(new ItemStack(Material.GOLD_INGOT)).onClick(history::pop);
-    }
 }
