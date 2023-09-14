ae6f7cae5172264ede354dc4172c46bee2254dc2
natan.nascimento <natanvnascimento@gmail.com>
2023-09-14 17:53:23 -0300

    Lint codebase

--- inventory-framework-anvil-input/src/main/java/me/devnatan/inventoryframework/AnvilInputFeature.java
+++ inventory-framework-anvil-input/src/main/java/me/devnatan/inventoryframework/AnvilInputFeature.java
@@ -1,33 +1,32 @@
 package me.devnatan.inventoryframework;
 
+import java.util.function.UnaryOperator;
 import me.devnatan.inventoryframework.feature.Feature;
 import org.jetbrains.annotations.NotNull;
 
-import java.util.function.UnaryOperator;
-
 public final class AnvilInputFeature<F extends IFViewFrame<?, ?>> implements Feature<Void, Void, F> {
 
-	/**
-	 * Instance of the Anvil Input feature.
-	 *
-	 * @see <a href="https://github.com/DevNatan/inventory-framework/wiki/anvil-input">Anvil Input on Wiki</a>
-	 */
-	public static final AnvilInputFeature<?> AnvilInput = new AnvilInputFeature<>();
+    /**
+     * Instance of the Anvil Input feature.
+     *
+     * @see <a href="https://github.com/DevNatan/inventory-framework/wiki/anvil-input">Anvil Input on Wiki</a>
+     */
+    public static final AnvilInputFeature<?> AnvilInput = new AnvilInputFeature<>();
 
-	private AnvilInputFeature() {}
+    private AnvilInputFeature() {}
 
-	@Override
-	public @NotNull String name() {
-		return "Anvil Input";
-	}
+    @Override
+    public @NotNull String name() {
+        return "Anvil Input";
+    }
 
-	@Override
-	public @NotNull Void install(F framework, UnaryOperator<Void> configure) {
-		return null;
-	}
+    @Override
+    public @NotNull Void install(F framework, UnaryOperator<Void> configure) {
+        return null;
+    }
 
-	@Override
-	public void uninstall(F framework) {
-		// TODO Unregister interceptors
-	}
+    @Override
+    public void uninstall(F framework) {
+        // TODO Unregister interceptors
+    }
 }
