5d9b1875adee0e0c6ee0c63b905062c96b775259
natan.nascimento <natanvnascimento@gmail.com>
2023-11-15 12:44:41 -0300

    Rename BukkitComponent to BukkitPlatformComponent

--- inventory-framework-platform-bukkit/src/main/java/me/devnatan/inventoryframework/component/BukkitComponent.java
+++
@@ -1,7 +0,0 @@
-package me.devnatan.inventoryframework.component;
-
-import me.devnatan.inventoryframework.context.Context;
-
-public abstract class BukkitComponent<B extends ComponentBuilder<B>>
-	extends PlatformComponent<Context, B, BukkitItemComponentBuilder> {
-}
---
+++ inventory-framework-platform-bukkit/src/main/java/me/devnatan/inventoryframework/component/BukkitPlatformComponent.java
@@ -0,0 +1,6 @@
+package me.devnatan.inventoryframework.component;
+
+import me.devnatan.inventoryframework.context.Context;
+
+public abstract class BukkitPlatformComponent<B extends ComponentBuilder<B>> extends PlatformComponent<Context, B> {
+}
