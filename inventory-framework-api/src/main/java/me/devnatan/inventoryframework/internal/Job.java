ff654af5a04b759f6b0c846995a944d04d578a83
natan.nascimento <natanvnascimento@gmail.com>
2023-11-19 12:22:28 -0300

    Delete unused InternalJobImpl and remove default from loop

--- inventory-framework-api/src/main/java/me/devnatan/inventoryframework/internal/Job.java
+++ inventory-framework-api/src/main/java/me/devnatan/inventoryframework/internal/Job.java
@@ -1,7 +1,6 @@
 package me.devnatan.inventoryframework.internal;
 
 import org.jetbrains.annotations.ApiStatus;
-import org.jetbrains.annotations.TestOnly;
 
 public interface Job {
 
@@ -29,46 +28,5 @@ public interface Job {
      * this library. No compatibility guarantees are provided. </i></b>
      */
     @ApiStatus.Internal
-    default void loop() {}
-
-    @ApiStatus.Internal
-    class InternalJobImpl implements Job {
-
-        private final Runnable job;
-        private boolean started;
-
-        public InternalJobImpl(Runnable job) {
-            this.job = job;
-        }
-
-        @Override
-        public String toString() {
-            return "InternalJobImpl{" + "job=" + job + ", started=" + started + '}';
-        }
-
-        @Override
-        public boolean isStarted() {
-            return started;
-        }
-
-        @Override
-        public void start() {
-            this.started = true;
-        }
-
-        @Override
-        public void cancel() {
-            this.started = false;
-        }
-
-        @Override
-        public void loop() {
-            job.run();
-        }
-
-        @TestOnly
-        public void setStarted(boolean started) {
-            this.started = started;
-        }
-    }
+    void loop();
 }
