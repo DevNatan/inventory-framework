dca4f4dd2a27dc270c0b8627d958f36c58d53e86
natan.nascimento <natanvnascimento@gmail.com>
2023-11-19 11:40:37 -0300

    Check if context is cancelled before stop scheduled update

--- inventory-framework-core/src/main/java/me/devnatan/inventoryframework/pipeline/ScheduledUpdateAfterCloseInterceptor.java
+++ inventory-framework-core/src/main/java/me/devnatan/inventoryframework/pipeline/ScheduledUpdateAfterCloseInterceptor.java
@@ -3,7 +3,6 @@ package me.devnatan.inventoryframework.pipeline;
 import me.devnatan.inventoryframework.RootView;
 import me.devnatan.inventoryframework.VirtualView;
 import me.devnatan.inventoryframework.context.IFCloseContext;
-import me.devnatan.inventoryframework.context.IFContext;
 
 public final class ScheduledUpdateAfterCloseInterceptor implements PipelineInterceptor<VirtualView> {
 
@@ -11,7 +10,9 @@ public final class ScheduledUpdateAfterCloseInterceptor implements PipelineInter
     public void intercept(PipelineContext<VirtualView> pipeline, VirtualView subject) {
         if (!(subject instanceof IFCloseContext)) return;
 
-        final IFContext context = (IFContext) subject;
+        final IFCloseContext context = (IFCloseContext) subject;
+        if (context.isCancelled()) return;
+
         final RootView root = context.getRoot();
         if (root.getScheduledUpdateJob() == null
                 || !root.getScheduledUpdateJob().isStarted()) return;
