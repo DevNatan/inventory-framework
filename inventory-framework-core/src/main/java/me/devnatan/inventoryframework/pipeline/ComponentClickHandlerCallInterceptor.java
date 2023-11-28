6f618475c83febbef2dd28c25cd87660a815e8be
Natan Vieira <natanvnascimento@gmail.com>
2023-11-28 11:44:45 -0300

    Call component interaction handler in core interceptors (#542)

---
+++ inventory-framework-core/src/main/java/me/devnatan/inventoryframework/pipeline/ComponentClickHandlerCallInterceptor.java
@@ -0,0 +1,21 @@
+package me.devnatan.inventoryframework.pipeline;
+
+import me.devnatan.inventoryframework.VirtualView;
+import me.devnatan.inventoryframework.component.Component;
+import me.devnatan.inventoryframework.context.IFSlotClickContext;
+import org.jetbrains.annotations.NotNull;
+
+public final class ComponentClickHandlerCallInterceptor implements PipelineInterceptor<VirtualView> {
+
+    @Override
+    public void intercept(@NotNull PipelineContext<VirtualView> pipeline, @NotNull VirtualView subject) {
+        if (!(subject instanceof IFSlotClickContext)) return;
+
+        final IFSlotClickContext click = (IFSlotClickContext) subject;
+        final Component component = click.getComponent();
+        if (component == null) return;
+        if (component.getInteractionHandler() == null) return;
+
+        component.getInteractionHandler().clicked(component, click);
+    }
+}
