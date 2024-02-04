ddf54f9e3473fe8165225b6e90d4d589ff5e74f7
Natan Vieira <natanvnascimento@gmail.com>
2024-02-04 17:18:39 -0300

    Create ViewFrame register(views) overload (#626)

--- inventory-framework-platform-bukkit/src/main/java/me/devnatan/inventoryframework/runtime/InventoryFramework.java
+++ inventory-framework-platform-bukkit/src/main/java/me/devnatan/inventoryframework/runtime/InventoryFramework.java
@@ -1,8 +1,27 @@
 package me.devnatan.inventoryframework.runtime;
 
+import me.devnatan.inventoryframework.View;
+import me.devnatan.inventoryframework.ViewFrame;
+import me.devnatan.inventoryframework.context.RenderContext;
+import org.bukkit.Material;
+import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.jetbrains.annotations.NotNull;
 
 public final class InventoryFramework extends JavaPlugin {
 
     public static final String LIBRARY_VERSION = "3.1.0-beta";
+
+    @Override
+    public void onEnable() {
+        ViewFrame.create(this).enableDebug().with(new Test()).register();
+    }
+}
+
+class Test extends View {
+
+    @Override
+    public void onFirstRender(@NotNull RenderContext render) {
+        render.firstSlot().withItem(new ItemStack(Material.GOLD_INGOT));
+    }
 }
