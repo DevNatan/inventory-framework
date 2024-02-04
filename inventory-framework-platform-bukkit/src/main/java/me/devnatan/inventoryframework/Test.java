f272476dbfa05ed554564841f0f867bd9e3b3428
natan.nascimento <natanvnascimento@gmail.com>
2024-02-04 17:11:31 -0300

    Remove test code

--- inventory-framework-platform-bukkit/src/main/java/me/devnatan/inventoryframework/Test.java
+++
@@ -1,7 +0,0 @@
-package me.devnatan.inventoryframework;
-
-import me.devnatan.inventoryframework.component.BukkitComponent;
-
-public class Test extends View {}
-
-class MyComponent extends BukkitComponent {}
