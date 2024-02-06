e4b0c2222cfd4292a89a1a9929a9db726401e046
Natan Vieira <natanvnascimento@gmail.com>
2024-02-06 15:29:06 -0300

    Publish snapshot versions (#639)

--- inventory-framework-platform-bukkit/src/main/java/me/devnatan/inventoryframework/runtime/InventoryFramework.java
+++ inventory-framework-platform-bukkit/src/main/java/me/devnatan/inventoryframework/runtime/InventoryFramework.java
@@ -2,11 +2,4 @@ package me.devnatan.inventoryframework.runtime;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class InventoryFramework extends JavaPlugin {
-
-    public static final String LIBRARY_VERSION = "3.0.9";
-
-    @Override
-    public void onEnable() {
-    }
-}
+public final class InventoryFramework extends JavaPlugin {}
