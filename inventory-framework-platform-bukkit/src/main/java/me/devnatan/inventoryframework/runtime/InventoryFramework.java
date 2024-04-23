a63d56e8126c3a264b0f5580325f38df1b3fe2cd
Natan Vieira <natanvnascimento@gmail.com>
2024-04-22 22:52:52 -0300

    Fix build version

--- inventory-framework-platform-bukkit/src/main/java/me/devnatan/inventoryframework/runtime/InventoryFramework.java
+++ inventory-framework-platform-bukkit/src/main/java/me/devnatan/inventoryframework/runtime/InventoryFramework.java
@@ -2,26 +2,44 @@ package me.devnatan.inventoryframework.runtime;
 
 import me.devnatan.inventoryframework.View;
 import me.devnatan.inventoryframework.ViewFrame;
-import me.devnatan.inventoryframework.context.RenderContext;
-import org.bukkit.Material;
-import org.bukkit.inventory.ItemStack;
+import me.devnatan.inventoryframework.context.CloseContext;
+import me.devnatan.inventoryframework.context.OpenContext;
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.jetbrains.annotations.NotNull;
 
 public final class InventoryFramework extends JavaPlugin {
 
-    public static final String LIBRARY_VERSION = "3.1.0-beta";
+    public static final String LIBRARY_VERSION = "3.0.9";
 
     @Override
     public void onEnable() {
-        ViewFrame.create(this).enableDebug().with(new Test()).register();
+        ViewFrame.create(this).enableDebug().with(new A(), new B()).register();
     }
 }
 
-class Test extends View {
+class A extends View {
 
     @Override
-    public void onFirstRender(@NotNull RenderContext render) {
-        render.firstSlot().withItem(new ItemStack(Material.GOLD_INGOT));
+    public void onOpen(@NotNull OpenContext open) {
+        Bukkit.broadcastMessage("[A] onOpen");
+    }
+
+    @Override
+    public void onClose(@NotNull CloseContext close) {
+        Bukkit.broadcastMessage("[A] onClose");
+    }
+}
+
+class B extends View {
+
+    @Override
+    public void onOpen(@NotNull OpenContext open) {
+        Bukkit.broadcastMessage("[B] onOpen");
+    }
+
+    @Override
+    public void onClose(@NotNull CloseContext close) {
+        Bukkit.broadcastMessage("[B] onClose");
     }
 }
