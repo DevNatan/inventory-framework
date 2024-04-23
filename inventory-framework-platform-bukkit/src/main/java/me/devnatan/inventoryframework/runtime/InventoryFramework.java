e5e48b21de0f24b77768418d8376e4de5aa2c601
Natan Vieira <natanvnascimento@gmail.com>
2024-04-23 16:36:22 -0300

    Remove test code

--- inventory-framework-platform-bukkit/src/main/java/me/devnatan/inventoryframework/runtime/InventoryFramework.java
+++ inventory-framework-platform-bukkit/src/main/java/me/devnatan/inventoryframework/runtime/InventoryFramework.java
@@ -1,12 +1,6 @@
 package me.devnatan.inventoryframework.runtime;
 
-import me.devnatan.inventoryframework.View;
-import me.devnatan.inventoryframework.ViewFrame;
-import me.devnatan.inventoryframework.context.CloseContext;
-import me.devnatan.inventoryframework.context.OpenContext;
-import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
-import org.jetbrains.annotations.NotNull;
 
 public final class InventoryFramework extends JavaPlugin {
 
@@ -14,32 +8,5 @@ public final class InventoryFramework extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        ViewFrame.create(this).enableDebug().with(new A(), new B()).register();
-    }
-}
-
-class A extends View {
-
-    @Override
-    public void onOpen(@NotNull OpenContext open) {
-        Bukkit.broadcastMessage("[A] onOpen");
-    }
-
-    @Override
-    public void onClose(@NotNull CloseContext close) {
-        Bukkit.broadcastMessage("[A] onClose");
-    }
-}
-
-class B extends View {
-
-    @Override
-    public void onOpen(@NotNull OpenContext open) {
-        Bukkit.broadcastMessage("[B] onOpen");
-    }
-
-    @Override
-    public void onClose(@NotNull CloseContext close) {
-        Bukkit.broadcastMessage("[B] onClose");
     }
 }
