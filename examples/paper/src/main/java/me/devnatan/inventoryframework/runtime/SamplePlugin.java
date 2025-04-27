82c48ff6585e371c7d364c80dda6f5a4a34a4145
Natan <natanvnascimento@gmail.com>
2025-04-27 16:29:22 -0300

    feat: update samples

--- examples/paper/src/main/java/me/devnatan/inventoryframework/runtime/SamplePlugin.java
+++ examples/paper/src/main/java/me/devnatan/inventoryframework/runtime/SamplePlugin.java
@@ -3,10 +3,7 @@ package me.devnatan.inventoryframework.runtime;
 import me.devnatan.inventoryframework.AnvilInputFeature;
 import me.devnatan.inventoryframework.ViewFrame;
 import me.devnatan.inventoryframework.runtime.commands.IFExampleCommandExecutor;
-import me.devnatan.inventoryframework.runtime.view.AnvilInputSample;
-import me.devnatan.inventoryframework.runtime.view.AutoUpdate;
-import me.devnatan.inventoryframework.runtime.view.Failing;
-import me.devnatan.inventoryframework.runtime.view.SimplePagination;
+import me.devnatan.inventoryframework.runtime.view.*;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class SamplePlugin extends JavaPlugin {
@@ -16,7 +13,12 @@ public class SamplePlugin extends JavaPlugin {
         System.out.println("ligoo");
         ViewFrame viewFrame = ViewFrame.create(this)
                 .install(AnvilInputFeature.AnvilInput)
-                .with(new AnvilInputSample(), new Failing(), new SimplePagination(), new AutoUpdate())
+                .with(
+                        new AnvilInputSample(),
+                        new Failing(),
+                        new SimplePagination(),
+                        new AutoUpdate(),
+                        new AutoUpdateAdvanced())
                 .register();
 
         getCommand("ifexample").setExecutor(new IFExampleCommandExecutor(viewFrame));
