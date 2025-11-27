7af74eab7f24ee620f5063db9b47f54b31bcb7e8
Natan <natanvnascimento@gmail.com>
2025-11-27 11:49:23 -0300

    docs: `IFSlotContext#getComponent`

--- inventory-framework-api/src/main/java/me/devnatan/inventoryframework/context/IFSlotContext.java
+++ inventory-framework-api/src/main/java/me/devnatan/inventoryframework/context/IFSlotContext.java
@@ -62,14 +62,14 @@ public interface IFSlotContext extends IFContext {
     @NotNull
     ViewContainer getContainer();
 
-    /**
-     * The component placed at that slot.
-     *
-     * <p><b><i>This is an internal inventory-framework API that should not be used from outside of
-     * this library. No compatibility guarantees are provided.</i></b>
-     *
-     * @return The component placed at that slot.
-     */
+	/**
+	 * The component associated with this slot context.
+	 *
+	 * <p><b><i>This is an internal inventory-framework API that should not be used from outside of
+	 * this library. No compatibility guarantees are provided.</i></b>
+	 *
+	 * @return The component associated with this slot context.
+	 */
     @ApiStatus.Internal
     @UnknownNullability
     Component getComponent();
